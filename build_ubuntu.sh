0904d17d104d3da01fdd6fd003f091d2a1306483
vitamin-caig <vitamin.caig@gmail.com>
2011-02-08 19:03:07 +0000

    Updates in build_ubuntu script.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@937 e19a5227-2556-0410-816f-a35683127991

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -23,45 +23,64 @@ case ${Arch} in
     ppc | ppc64 | powerpc)
       test `grep cpu /proc/cpuinfo | uniq | cut -f 3 -d " "` = "altivec" && cxx_flags="-mabi=altivec -maltivec"
       test `grep cpu /proc/cpuinfo | uniq | cut -f 2 -d " " | sed -e 's/,//g'` = "PPC970MP" && cxx_flags="${cxx_flags} -mtune=970 -mcpu=970"
-      arch=powerpc
+      PkgArch=powerpc
     ;;
     x86_64)
       cxx_flags="-m64 -march=x86-64 -mtune=generic -mmmx"
       ld_flags="-m64"
-      arch=amd64
+      PkgArch=amd64
     ;;
     i386 | i486 | i586 | i686)
       cxx_flags="-m32 -march=${Arch} -mtune=generic -mmmx"
       ld_flags="-m32"
-      arch=i386
+      PkgArch=i386
     ;;
     *)
     ;;
 esac
 
-makecmd="make platform=linux release=1 arch=${arch}-ubuntu"
+makecmd="make platform=linux release=1 arch=${Arch}-ubuntu"
 ${makecmd} -j ${cpus} all \
-defines="ZXTUNE_VERSION=rev${Revision} BUILD_PLATFORM=linux BUILD_ARCH=${arch}-ubuntu" \
+defines="ZXTUNE_VERSION=rev${Revision} BUILD_PLATFORM=linux BUILD_ARCH=${Arch}-ubuntu" \
 cxx_flags="${cxx_flags}" ld_flags="${ld_flags}" \
 -C apps/zxtune-qt | gzip -9 -c > ${BuildDir}/build.log.gz
 
 PkgDir=${BuildDir}/pkg
+mkdir -p ${PkgDir}
+DESTDIR=`pwd`/${PkgDir} ${makecmd} install -C apps/zxtune-qt > ${BuildDir}/install.log
+mkdir -p ${PkgDir}/usr/share/doc/zxtune-qt
+echo "\
+ZXTune\n\
+\n\
+Copyright 2009-2011 Vitamin <vitamin.caig@gmail.com>\n\
+\n\
+Visit http://zxtune.googlecode.com to get new versions and report about errors.\n\
+\n\
+This software is distributed under GPLv3 license.\n"\
+> ${PkgDir}/usr/share/doc/zxtune-qt/copyright
+echo "\
+zxtune-qt (r${Revision}) experimental; urgency=low\n\
+\n\
+  * See svn log for details\n\
+ -- Vitamin <vitamin.caig@gmail.com>  "`date -R`\
+> ${PkgDir}/usr/share/doc/zxtune-qt/changelog
+gzip --best ${PkgDir}/usr/share/doc/zxtune-qt/changelog
+
+PkgFiles=`find ${PkgDir} -type f`
 DebDir=${PkgDir}/DEBIAN
 mkdir -p ${DebDir}
-DESTDIR=`pwd`/${PkgDir} ${makecmd} install -C apps/zxtune-qt > ${BuildDir}/install.log
-PkgFiles=`find ${PkgDir} -type f | grep -v '^${DebDir}/'`
-touch ${DebDir}/changelog
-echo 7 > ${DebDir}/compat
-md5sum ${PkgFiles} > ${DebDir}/md5sums
+md5sum ${PkgFiles} | sed 's| .*pkg\/| |' > ${DebDir}/md5sums
 echo "\
 Package: zxtune-qt\n\
-Version: r${Revision}\n\
-Architecture: ${arch}\n\
+Version: ${Revision}\n\
+Architecture: ${PkgArch}\n\
 Priority: optional\n\
 Section: sound\n\
+Maintainer: Vitamin <vitamin.caig@gmail.com>\n\
+Depends: libc6, libboost-thread1.40.0, libqtgui4 (>= 4.6.0), libqtcore4 (>= 4.6.0), libasound2\n\
 Description: QT4-based application based on ZXTune library\n\
-Maintainer: vitamin.caig@gmail.com\n\
-Depends: libboost-thread1.40.0, libqtgui4 (>= 4.6.0), libqtcore4 (>= 4.6.0), libasound2\n\
-Installed-Size: "`du -cs ${PkgFiles} | tail -n 1 | cut -f1`"\n" > ${DebDir}/control
+ This package provides zxtune-qt application\n\
+ used to play chiptunes from ZX Spectrum.\n"\
+> ${DebDir}/control
 
-dpkg-deb -b ${PkgDir} ${BuildDir}/zxtune-qt_r${Revision}_${Arch}-ubuntu.deb
+fakeroot dpkg-deb --build ${PkgDir} ${BuildDir}/zxtune-qt_r${Revision}_${Arch}-ubuntu.deb
