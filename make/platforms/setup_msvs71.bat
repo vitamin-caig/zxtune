77428a152c945690b9f4d117d41d17b2377dabd2
vitamin-caig <vitamin.caig@gmail.com>
2011-04-14 08:19:55 +0000

    Removing VisualStudio version from build tags.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1064 e19a5227-2556-0410-816f-a35683127991

--- make/platforms/setup_msvs71.bat
+++ make/platforms/setup_msvs71.bat
@@ -6,7 +6,7 @@ ECHO %PATH% | FIND "%VS_PATH%" > NUL && GOTO Quit
 call "%VS_PATH%\Common7\Tools\vsvars32.bat"
 SET MSVS_VERSION=vc71
 
-SET BUILD_ARCH=%MSVS_VERSION%_x86
+SET BUILD_ARCH=x86
 SET self=%0%
 call %self:setup_msvs71=setup_build%
 call %self:setup_msvs71=setup_boost%
--- make/platforms/setup_msvs80_x64.bat
+++ make/platforms/setup_msvs80_x64.bat
@@ -6,7 +6,7 @@ ECHO %PATH% | FIND "%VS_PATH%" > NUL && GOTO Quit
 call "%VS_PATH%\VC\vcvarsall.bat" x86_amd64
 SET MSVS_VERSION=vc80
 
-SET BUILD_ARCH=%MSVS_VERSION%_x86_64
+SET BUILD_ARCH=x86_64
 SET self=%0%
 call %self:setup_msvs80_x64=setup_build%
 call %self:setup_msvs80_x64=setup_boost% 64
