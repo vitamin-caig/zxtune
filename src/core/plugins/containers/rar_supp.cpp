300c103ed3a0163285ff97772ab56ecb5741155e
vitamin-caig <vitamin.caig@gmail.com>
2011-09-07 12:07:52 +0000

    Initial packed RARv2 files support.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1303 e19a5227-2556-0410-816f-a35683127991

--- src/core/plugins/containers/rar_supp.cpp
+++ src/core/plugins/containers/rar_supp.cpp
@@ -13,6 +13,7 @@ Author:
 #include "container_supp_common.h"
 #include "core/plugins/registrator.h"
 #include "core/src/path.h"
+#include "formats/packed/pack_utils.h"
 //common includes
 #include <byteorder.h>
 #include <logging.h>
@@ -23,13 +24,12 @@ Author:
 #include <formats/packed_decoders.h>
 //std includes
 #include <numeric>
+#include <cstring>
 //boost includes
+#include <boost/array.hpp>
 #include <boost/make_shared.hpp>
 //text includes
 #include <core/text/plugins.h>
-
-//platform includes
-#include <zlib.h>
 
 namespace
 {
@@ -54,7 +54,10 @@ namespace
 
     enum
     {
-      FLAG_HAS_TAIL = 0x8000
+      FLAG_HAS_TAIL = 0x8000,
+
+      MIN_TYPE = 0x72,
+      MAX_TYPE = 0x7b
     };
 
     bool IsExtended() const
@@ -187,6 +190,526 @@ namespace
     const std::size_t DestSize;
   };
 
+  const uint_t NC = 298;
+  const uint_t DC = 48;
+  const uint_t RC = 28;
+  const uint_t BC = 19;
+  const uint_t MC = 257;
+
+  struct BaseDecodeTree
+  {
+    boost::array<uint_t, 16> DecodeLen;
+    boost::array<uint_t, 16>  DecodePos;
+
+    BaseDecodeTree()
+      : DecodeLen()
+      , DecodePos()
+    {
+    }
+  };
+
+  template<uint_t numSize>
+  struct DecodeTree : BaseDecodeTree
+  {
+    boost::array<uint_t, numSize> DecodeNum;
+
+    DecodeTree()
+      : BaseDecodeTree()
+      , DecodeNum()
+    {
+    }
+  };
+
+  typedef DecodeTree<RC> RepDecodeTree;
+  typedef DecodeTree<BC> BitDecodeTree;
+  typedef DecodeTree<NC> LitDecodeTree;
+  typedef DecodeTree<DC> DistDecodeTree;
+  typedef DecodeTree<MC> MultDecodeTree;
+
+  class RarBitstream : public ByteStream
+  {
+  public:
+    RarBitstream(const uint8_t* data, std::size_t size)
+      : ByteStream(data, size)
+      , LRange()
+      , LCode()
+      , BitField()
+    {
+      AddBits(0);
+    }
+
+    void AddBits(uint_t bits)
+    {
+      LRange -= bits;
+      while (LRange < 24)
+      {
+        LCode = (LCode << 8) | GetByte();
+        LRange += 8;
+      }
+    }
+
+    void GetBits(uint_t bits)
+    {
+      GetBits();
+      AddBits(bits);
+    }
+
+    void GetBits()
+    {
+      BitField = LCode << (32 - LRange);
+    }
+
+    uint32_t GetBitField() const
+    {
+      return BitField;
+    }
+
+    template<uint_t numSize>
+    uint_t DecodeNumber(const DecodeTree<numSize>& tree)
+    {
+      const uint_t idx = DecodeNumIndex(tree);
+      return tree.DecodeNum[idx];
+    }
+  private:
+    uint_t DecodeNumIndex(const BaseDecodeTree& tree)
+    {
+      GetBits();
+      const uint_t len = (GetBitField() >> 16) & 0xfffe;
+      uint_t bits = 1;
+      while (bits < 15 && len >= tree.DecodeLen[bits])
+      {
+        ++bits;
+      }
+      AddBits(bits);
+      return tree.DecodePos[bits] + ((len - tree.DecodeLen[bits - 1]) >> (16 - bits));
+    }
+  private:
+    uint32_t LRange;
+    uint32_t LCode;
+    uint32_t BitField;
+  };
+
+  class RarDecoder
+  {
+  public:
+    RarDecoder(const uint8_t* const start, std::size_t size, std::size_t destSize)
+      : Stream(start, size)
+      , TargetSize(destSize)
+    {
+    }
+
+    Dump* GetDecodedData()
+    {
+      if (Decoded.empty())
+      {
+        Decode();
+      }
+      return !Decoded.empty()
+        ? &Decoded
+        : 0;
+    }
+  private:
+    void Decode()
+    {
+      static const uint_t LDecode[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 20, 24, 28, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224}; 
+      static const uint_t LBits[] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4,  4,  5,  5,  5,  5};
+      static const uint_t DDecode[] = 
+      {
+        0, 1, 2, 3, 4, 6, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096, 6144, 8192, 12288, 16384, 24576, 32768,
+        49152, 65536, 98304, 131072, 196608, 262144, 327680, 393216, 458752, 524288, 589824, 655360, 720896, 786432, 851968, 917504, 983040
+      }; 
+      static const uint_t DBits[] =
+      {
+        0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15,
+        16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16
+      };
+      static const uint_t SDDecode[] = {0, 4, 8, 16, 32, 64, 128, 192};
+      static const uint_t SDBits[] = {2, 2, 3, 4, 5, 6, 6, 6};
+
+      const std::size_t prevSize = Decoded.size();
+      const std::size_t requiredSize = prevSize + TargetSize;
+      Decoded.reserve(requiredSize);
+      ReadTables();
+      while (Decoded.size() < requiredSize)
+      {
+        if (Audio.Unpack)
+        {
+          const uint_t num = Stream.DecodeNumber(MD[Audio.CurChannel]);
+          if (256 == num)
+          {
+            ReadTables();
+            continue;
+          }
+          Decoded.push_back(DecodeAudio(num));
+          Audio.NextChannel();
+          continue;
+        }
+        const uint_t num = Stream.DecodeNumber(LD);
+        if (num < 256)
+        {
+          Decoded.push_back(static_cast<uint8_t>(num));
+          continue;
+        }
+        if (num > 269)
+        {
+          const uint_t lenIdx = num - 270;
+          uint_t len = LDecode[lenIdx] + 3;
+          if (const uint_t lenBits = LBits[lenIdx])
+          {
+            Stream.GetBits();
+            len += Stream.GetBitField() >> (32 - lenBits);
+            Stream.AddBits(lenBits);
+          }
+          const uint_t distIdx = Stream.DecodeNumber(DD);
+          uint_t dist = DDecode[distIdx] + 1;
+          if (const uint_t distBits = DBits[distIdx])
+          {
+            Stream.GetBits();
+            dist += Stream.GetBitField() >> (32 - distBits);
+            Stream.AddBits(distBits);
+          }
+          if (dist >= 0x40000L)
+          {
+            ++len;
+          }
+          if (dist >= 0x2000L)
+          {
+            ++len;
+          }
+          CopyString(dist, len);
+          continue;
+        }
+        if (num == 269)
+        {
+          ReadTables();
+          continue;
+        }
+        if (num == 256)
+        {
+          CopyString(History.LastDist, History.LastCount);
+          continue;
+        }
+        if (num < 261)
+        {
+          const uint_t dist = History.GetDist(num - 256);
+          const uint_t lenIdx = Stream.DecodeNumber(RD);
+          uint_t len = LDecode[lenIdx] + 2;
+          if (const uint_t lenBits = LBits[lenIdx])
+          {
+            Stream.GetBits();
+            len += Stream.GetBitField() >> (32 - lenBits);
+            Stream.AddBits(lenBits);
+          }
+          if (dist >= 0x40000L)
+          {
+            ++len;
+          }
+          if (dist >= 0x2000)
+          {
+            ++len;
+          }
+          if (dist >= 0x101)
+          {
+            ++len;
+          }
+          CopyString(dist, len);
+          continue;
+        }
+        if (num < 270)
+        {
+          const uint_t distIdx = num - 261;
+          uint_t dist = SDDecode[distIdx] + 1;
+          if (const uint_t distBits = SDBits[distIdx])
+          {
+            Stream.GetBits();
+            dist += Stream.GetBitField() >> (32 - distBits);
+            Stream.AddBits(distBits);
+          }
+          CopyString(dist, 2);
+          continue;
+        }
+      }
+      ReadLastTables();
+    }
+
+    void ReadTables()
+    {
+      Stream.GetBits(2);
+      Audio.Unpack = 0 != (Stream.GetBitField() & 0x80000000);
+      if (0 == (Stream.GetBitField() & 0x40000000))
+      {
+        UnpOldTable.assign(0);
+      }
+
+      if (Audio.Unpack)
+      {
+        Audio.SetChannels(((Stream.GetBitField() >> 28) & 3) + 1);
+        Stream.AddBits(2);
+      }
+      const uint_t tableSize = Audio.Unpack
+        ? MC * Audio.Channels
+        : NC + DC + RC;
+
+      boost::array<uint_t, BC> bitCount =  { {0} };
+      for (uint_t i = 0; i < bitCount.size(); ++i)
+      {
+        Stream.GetBits(4);
+        bitCount[i] = Stream.GetBitField() >> 28;
+      }
+      MakeDecodeTable(&bitCount[0], BD);
+      boost::array<uint_t, MC * 4> table = { {0} };
+      assert(tableSize <= table.size());
+      for (uint_t i = 0; i < tableSize; )
+      {
+        const uint_t number = Stream.DecodeNumber(BD);
+        if (number < 16)
+        {
+          table[i] = (number + UnpOldTable[i]) & 0xf;
+          ++i;
+        }
+        else if (number == 16)
+        {
+          Stream.GetBits(2);
+          for (uint_t n = (Stream.GetBitField() >> 30) + 3; n && i < tableSize; ++i, --n)
+          {
+            table[i] = table[i - 1];
+          }
+        }
+        else
+        {
+          int_t n = 17 == number
+            ? (Stream.GetBits(3), (Stream.GetBitField() >> 29) + 3)
+            : (Stream.GetBits(7), (Stream.GetBitField() >> 25) + 11);
+          for (; n-- > 0 && i < tableSize; ++i)
+          {
+            table[i] = 0;
+          }
+        }
+      }
+
+      if (Audio.Unpack)
+      {
+        for (uint_t i = 0; i < Audio.Channels; ++i)
+        {
+          MakeDecodeTable(&table[i * MC], MD[i]);
+        }
+      }
+      else
+      {
+        MakeDecodeTable(&table[0], LD);
+        MakeDecodeTable(&table[NC], DD);
+        MakeDecodeTable(&table[NC + DC], RD);
+      }
+    }
+
+    void ReadLastTables()
+    {
+      if (Audio.Unpack)
+      {
+        const uint_t num = Stream.DecodeNumber(MD[Audio.CurChannel]);
+        if (256 == num)
+        {
+          ReadTables();
+        }
+      }
+      else
+      {
+        const uint_t num = Stream.DecodeNumber(LD);
+        if (269 == num)
+        {
+          ReadTables();
+        }
+      }
+    }
+
+    template<uint_t treeSize>
+    void MakeDecodeTable(const uint_t* lenTab, DecodeTree<treeSize>& tree)
+    {
+      boost::array<uint_t, 16> lenCount = { {0} };
+
+      for (uint_t i = 0; i < treeSize; ++i)
+      {
+        ++lenCount[lenTab[i] & 0x0f];
+      }
+      lenCount[0] = 0;
+
+      FillTreeDecodeLen(&lenCount[0], tree);
+      boost::array<uint_t, 16> tmpPos = tree.DecodePos;
+
+      for (uint_t i = 0; i < treeSize; ++i)
+      {
+        if (lenTab[i])
+        {
+          tree.DecodeNum[tmpPos[lenTab[i] & 0xf]++] = i;
+        }
+      }
+    }
+
+    void FillTreeDecodeLen(const uint_t* lenCount, BaseDecodeTree& tree)
+    {
+      tree.DecodePos[0] = tree.DecodeLen[0] = 0;
+      for (uint_t n = 0, i = 1; i < 16; ++i)
+      {
+        n = 2 * (n + lenCount[i]);
+        tree.DecodeLen[i] = std::min<uint_t>(n << (15 - i), 0xffff);
+        tree.DecodePos[i] = tree.DecodePos[i - 1] + lenCount[i - 1];
+      }
+    }
+
+    void CopyString(uint_t dist, uint_t count)
+    {
+      CopyFromBack(dist, Decoded, count);
+      History.Store(dist, count);
+    }
+
+    uint8_t DecodeAudio(int_t delta)
+    {
+      MultimediaCompression::VarType& var = Audio.Variables[Audio.CurChannel];
+      ++var.ByteCount;
+      var.D[3] = var.D[2];
+      var.D[2] = var.D[1];
+      var.D[1] = var.LastDelta - var.D[0];
+      var.D[0] = var.LastDelta;
+      const int predicted = 
+        ((8 * var.LastChar + 
+          var.K[0] * var.D[0] + 
+          var.K[1] * var.D[1] + 
+          var.K[2] * var.D[2] + 
+          var.K[3] * var.D[3] +
+          var.K[4] * Audio.ChannelDelta) >> 3) & 0xff;
+      const int_t val = predicted - delta;
+      const int_t idx = static_cast<int8_t>(delta) << 3;
+      var.Dif[0] += absolute(idx);
+      var.Dif[1] += absolute(idx - var.D[0]);
+      var.Dif[2] += absolute(idx + var.D[0]);
+      var.Dif[3] += absolute(idx - var.D[1]);
+      var.Dif[4] += absolute(idx + var.D[1]);
+      var.Dif[5] += absolute(idx - var.D[2]);
+      var.Dif[6] += absolute(idx + var.D[2]);
+      var.Dif[7] += absolute(idx - var.D[3]);
+      var.Dif[8] += absolute(idx + var.D[3]);
+      var.Dif[9] += absolute(idx - Audio.ChannelDelta);
+      var.Dif[10] += absolute(idx + Audio.ChannelDelta);
+
+      Audio.ChannelDelta = var.LastDelta = static_cast<int8_t>(val - var.LastChar);
+      var.LastChar = val;
+      if (var.ByteCount & 0x1f)
+      {
+        return static_cast<uint8_t>(val);
+      }
+      const boost::array<uint_t, 11>::const_iterator minDifIt = std::min_element(var.Dif.begin(), var.Dif.end());
+      var.Dif.assign(0);
+      if (const std::size_t minIdx = minDifIt - var.Dif.begin())
+      {
+        int_t& k = var.K[(minIdx - 1) / 2];
+        if (minIdx & 1)
+        {
+          if (k >= -16)
+          {
+            --k;
+          }
+        }
+        else
+        {
+          if (k < 16)
+          {
+            ++k;
+          }
+        }
+      }
+      return static_cast<uint8_t>(val);
+    }
+  private:
+    RarBitstream Stream;
+    const std::size_t TargetSize;
+  
+    struct MultimediaCompression
+    {
+      bool Unpack;
+      uint_t Channels;
+      uint_t CurChannel;
+      int_t ChannelDelta;
+
+      struct VarType
+      {
+        boost::array<int_t, 5> K;
+        boost::array<int_t, 4> D;
+        int_t LastDelta;
+        boost::array<uint_t, 11> Dif;
+        uint_t ByteCount;
+        int_t LastChar;
+
+        VarType()
+          : K()
+          , D()
+          , LastDelta()
+          , Dif()
+          , ByteCount()
+          , LastChar()
+        {
+        }
+      } Variables[4];
+
+      MultimediaCompression()
+        : Unpack()
+        , Channels()
+        , CurChannel()
+        , ChannelDelta()
+      {
+      }
+
+      void SetChannels(uint_t count)
+      {
+        Channels = count;
+        if (CurChannel >= Channels)
+        {
+          CurChannel = 0;
+        }
+      }
+
+      void NextChannel()
+      {
+        if (++CurChannel >= Channels)
+        {
+          CurChannel = 0;
+        }
+      }
+    } Audio;
+    struct RefHistory
+    {
+      uint_t LastDist;
+      uint_t LastCount;
+      uint_t OldDistances[4];
+      uint_t CurHistory;
+
+      RefHistory()
+        : LastDist()
+        , LastCount()
+        , CurHistory()
+      {
+      }
+
+      void Store(uint_t dist, uint_t count)
+      {
+        LastDist = dist;
+        LastCount = count;
+        OldDistances[CurHistory++ & 3] = dist;
+      }
+
+      uint_t GetDist(uint_t depth) const
+      {
+        return OldDistances[(CurHistory - depth) & 3];
+      }
+    } History;
+    boost::array<uint8_t, MC * 4> UnpOldTable;
+    RepDecodeTree RD;
+    BitDecodeTree BD;
+    LitDecodeTree LD;
+    DistDecodeTree DD;
+    MultDecodeTree MD[4];
+    Dump Decoded;
+  };
+
   class PackedFile : public CompressedFile
   {
   public:
@@ -200,6 +723,13 @@ namespace
     virtual std::auto_ptr<Dump> Decompress() const
     {
       Log::Debug(THIS_MODULE, "Depack %1% -> %2%", Size, DestSize);
+      RarDecoder decoder(Start, Size, DestSize);
+      if (Dump* decoded = decoder.GetDecodedData())
+      {
+        std::auto_ptr<Dump> res(new Dump());
+        res->swap(*decoded);
+        return res;
+      }
       return std::auto_ptr<Dump>();
     }
   private:
@@ -220,6 +750,9 @@ namespace
       {
       case 0x30:
         Delegate.reset(new NoCompressedFile(start, size, outSize));
+        break;
+      default:
+        Delegate.reset(new PackedFile(start, size, outSize));
         break;
       }
     }
@@ -291,7 +824,7 @@ namespace
     bool IsEof() const
     {
       const uint64_t curBlockSize = GetBlockSize();
-      return uint64_t(Offset) + curBlockSize > uint64_t(Limit);
+      return !curBlockSize || uint64_t(Offset) + curBlockSize > uint64_t(Limit);
     }
 
     const BlockHeader& GetCurrentBlock() const
