f87223f7a73069ab910451c6c70a8bb6265c1095
vitamin-caig <vitamin.caig@gmail.com>
2011-09-30 21:45:50 +0000

    Added autotest for Hrip.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1363 e19a5227-2556-0410-816f-a35683127991

---
+++ src/formats/test/hrip/test.cpp
@@ -0,0 +1,49 @@
+#include "../utils.h"
+
+namespace
+{
+  void TestHripSmall()
+  {
+    Dump rar;
+    Test::OpenFile("test.hrp", rar);
+
+    Dump etalon;
+    Test::OpenFile("etalon16.bin", etalon);
+
+    const Formats::Packed::Decoder::Ptr packed = Formats::Packed::CreateHrust23Decoder();
+    std::map<std::string, Dump> tests;
+    const uint8_t* const data = &rar[0];
+    tests["test16k"] = Dump(data + 8, data + 0x21c0);
+    Test::TestPacked(*packed, etalon, tests);
+  }
+
+  void TestHripBig()
+  {
+    Dump rar;
+    Test::OpenFile("test.hrp", rar);
+
+    Dump etalon;
+    Test::OpenFile("etalon48.bin", etalon);
+
+    const Formats::Packed::Decoder::Ptr packed = Formats::Packed::CreateHrust23Decoder();
+    std::map<std::string, Dump> tests;
+    const uint8_t* const data = &rar[0];
+    tests["test48k"] = Dump(data + 0x21c0, data + 0x4821);
+    Test::TestPacked(*packed, etalon, tests);
+  }
+}
+
+
+int main()
+{
+  try
+  {
+    TestHripSmall();
+    TestHripBig();
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+}
