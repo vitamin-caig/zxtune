9c17beb15f5231627997a4c10b0d4d6937fb5aa7
vitamin-caig <vitamin.caig@gmail.com>
2011-10-24 06:53:15 +0000

    Upgrade MSVS builds to boost1.47 and qt4.7.4.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1392 e19a5227-2556-0410-816f-a35683127991

--- make/platforms/setup_msvs71.bat
+++ make/platforms/setup_msvs71.bat
@@ -6,6 +6,8 @@ ECHO %PATH% | FIND "%VS_PATH%" > NUL && GOTO Quit
 call "%VS_PATH%\Common7\Tools\vsvars32.bat"
 SET MSVS_VERSION=vc71
 
+SET BOOST_VERSION=1_47
+SET QT_VERSION=4.7.4
 SET BUILD_ARCH=x86
 SET self=%0%
 call %self:setup_msvs71=setup_build%
--- make/platforms/setup_msvs80_x64.bat
+++ make/platforms/setup_msvs80_x64.bat
@@ -6,6 +6,8 @@ ECHO %PATH% | FIND "%VS_PATH%" > NUL && GOTO Quit
 call "%VS_PATH%\VC\vcvarsall.bat" x86_amd64
 SET MSVS_VERSION=vc80
 
+SET BOOST_VERSION=1_47
+SET QT_VERSION=4.7.4
 SET BUILD_ARCH=x86_64
 SET self=%0%
 call %self:setup_msvs80_x64=setup_build%
