9a46bb0b971702fd6ea0dfade7907e24604ef156
vitamin-caig <vitamin.caig@gmail.com>
2012-07-03 09:02:37 +0000

    Fix mingw builds. Updated changelog and config file.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1849 e19a5227-2556-0410-816f-a35683127991

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ svn up > NUL || GOTO Error
 ECHO Cleaning
 make clean release=1 -C apps > NUL || GOTO Error
 
-make -j -l %NUMBER_OF_PROCESSORS% package release=1 -C apps && GOTO Exit
+make -j -l %NUMBER_OF_PROCESSORS% package release=1 -C apps/bundle && GOTO Exit
 :Error
 ECHO Failed
 SET
--- make/features.mak
+++ make/features.mak
@@ -1,3 +1,4 @@
 ifdef support_directsound
 windows_libraries += user32 ole32
+mingw_libraries += ole32
 endif
