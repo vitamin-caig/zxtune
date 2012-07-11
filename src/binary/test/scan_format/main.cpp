333e1311d51fcba76bf03cc330af84a036c215fb
vitamin-caig <vitamin.caig@gmail.com>
2012-07-11 06:17:16 +0000

    Added simple scan_format utility.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1865 e19a5227-2556-0410-816f-a35683127991

---
+++ src/binary/test/scan_format/main.cpp
@@ -0,0 +1,49 @@
+#include <types.h>
+#include <tools.h>
+#include <error_tools.h>
+#include <parameters.h>
+#include <binary/format.h>
+#include <io/provider.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  if (argc < 3)
+  {
+    std::cout << *argv << " <filename> <pattern>" << std::endl;
+    return 0;
+  }
+  try
+  {
+    const Binary::Format::Ptr format = Binary::Format::Create(argv[2]);
+    const std::string filename = argv[1];
+    const Parameters::Accessor::Ptr params = Parameters::Container::Create();
+    Binary::Container::Ptr data;
+    ThrowIfError(ZXTune::IO::OpenData(filename, *params, ZXTune::IO::ProgressCallback(), data));
+
+    if (format->Match(data->Data(), data->Size()))
+    {
+      std::cout << "Matched" << std::endl;
+    }
+    else
+    {
+      const std::size_t offset = format->Search(data->Data(), data->Size());
+      if (offset != data->Size())
+      {
+        std::cout << "Matched at offset " << offset << std::endl;
+      }
+      else
+      {
+        std::cout << "Not found" << std::endl;
+      }
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << std::endl;
+  }
+  catch (const Error& e)
+  {
+    std::cout << Error::ToString(e) << std::endl;
+  }
+}
