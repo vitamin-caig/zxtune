0aff48ce053e1111ae6f92325b9adcfce5ebdcac
vitamin-caig <vitamin.caig@gmail.com>
2012-09-09 09:05:35 +0000

    Adding missed qt build script.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1989 e19a5227-2556-0410-816f-a35683127991

---
+++ make/build/linux/qt/build_arm.sh
@@ -0,0 +1,15 @@
+QT_VERSION=`ls -1 changes* | sed -nr 's/changes-(.*)/\1/p'`
+ARCH=arm
+PLATFORM=linux-arm-g++
+echo "Bulding version ${QT_VERSION} for ${ARCH}"
+PREFIX=/mnt/devel/zxtune/Build/qt-${QT_VERSION}-linux-${ARCH}
+mkdir -p ${PREFIX}
+TARGETS="-nomake demos -nomake examples -nomake docs -nomake translations -nomake assistant -nomake qdoc3 -nomake qtracereplay -nomake linguist -nomake qt3to4 -nomake qcollectiongenerator -nomake pixeltool"
+VERSIONS="-opensource -release -static -fast -confirm-license"
+FORMATS="-no-gif -no-libtiff -no-libmng -no-libjpeg -qt-libpng"
+FEATURES="-no-accessibility -no-opengl -no-openvg -no-sql-sqlite -no-qt3support -no-openssl -no-nis -no-cups -no-qdbus -no-dbus -no-gtkstyle -no-glib \
+-fontconfig -xrender -xrandr -xfixes -xshape"
+PARTS="-no-webkit -no-javascript-jit -no-phonon -no-phonon-backend -no-multimedia -no-audio-backend -no-script -no-scripttools -no-declarative -no-declarative-debug -no-xmlpatterns"
+
+export PATH=/opt/arm-linux/bin:$PATH
+./configure -arch arm -xplatform ${PLATFORM} -prefix ${PREFIX} $TARGETS $STYLES $VERSIONS $FORMATS $FEATURES $PARTS && make -j `grep -c processor /proc/cpuinfo` && make install
