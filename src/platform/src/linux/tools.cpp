42d0136d85e20aa846f01c2e4e22b3adf4945bd4
vitamin-caig <vitamin.caig@gmail.com>
2012-09-10 13:30:05 +0000

    Fixed self executable path resolving on linux.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@1991 e19a5227-2556-0410-816f-a35683127991

--- src/platform/src/linux/tools.cpp
+++ src/platform/src/linux/tools.cpp
@@ -13,6 +13,8 @@ Author:
 #include <tools.h>
 //library includes
 #include <platform/tools.h>
+//boost includes
+#include <boost/format.hpp>
 //platform includes
 #include <dlfcn.h>
 #include <unistd.h>
@@ -30,15 +32,15 @@ namespace
   
   String GetExecutableName()
   {
-    static const std::string SELF_PATH("/proc/self/exe");
+    const std::string selfPath = (boost::format("/proc/%1%/exe") % ::getpid()).str();
     struct stat sb;
-    if (-1 == ::lstat(SELF_PATH.c_str(), &sb))
+    if (-1 == ::lstat(selfPath.c_str(), &sb))
     {
       return String();
     }
     
     std::vector<char> filename(sb.st_size + 1);
-    const int len = ::readlink("/proc/self/exe", &filename[0], filename.size() - 1);
+    const int len = ::readlink(selfPath.c_str(), &filename[0], filename.size() - 1);
     if (len > 0)
     {
       filename[len] = 0;
