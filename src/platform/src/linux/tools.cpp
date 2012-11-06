f68b3e671e8fac7e7d55d8d5312b676ebd957c06
vitamin-caig <vitamin.caig@gmail.com>
2012-11-06 16:27:06 +0000

    Fixed Platform::GetCurrentImageFilename if binary is located in PATH variable.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@2091 e19a5227-2556-0410-816f-a35683127991

--- src/platform/src/linux/tools.cpp
+++ src/platform/src/linux/tools.cpp
@@ -10,46 +10,76 @@ Author:
 */
 
 //common includes
+#include <debug_log.h>
 #include <tools.h>
 //library includes
 #include <platform/tools.h>
-//boost includes
-#include <boost/format.hpp>
 //platform includes
 #include <dlfcn.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/stat.h>
+
+namespace
+{
+  const Debug::Stream Dbg("Platform::Linux");
+}
 
 namespace
 {
   std::string GetSharedLibraryName()
   {
     Dl_info info;
-    return 0 != ::dladdr(reinterpret_cast<void*>(&GetSharedLibraryName), &info)
-      ? std::string(info.dli_fname)
-      : std::string();
-  }
-  
-  std::string GetExecutableName()
-  {
-    const std::string selfPath = (boost::format("/proc/%1%/exe") % ::getpid()).str();
-    struct stat sb;
-    if (-1 == ::lstat(selfPath.c_str(), &sb))
+    if (::dladdr(reinterpret_cast<void*>(&GetSharedLibraryName), &info) &&
+        info.dli_sname && info.dli_saddr)
     {
-      return std::string();
-    }
-    
-    std::vector<char> filename(sb.st_size + 1);
-    const int len = ::readlink(selfPath.c_str(), &filename[0], filename.size() - 1);
-    if (len > 0)
-    {
-      filename[len] = 0;
-      return std::string(&filename[0]);
+      const std::string result(info.dli_fname);
+      Dbg("Shared library name: %1%", result);
+      return result;
     }
     else
     {
       return std::string();
     }
+  }
+  
+  std::string GetExecutableName()
+  {
+    const std::string selfPath = "/proc/self/exe";
+    struct stat sb;
+    if (-1 == ::lstat(selfPath.c_str(), &sb))
+    {
+      Dbg("Failed to stat %1% (errno %2%)", selfPath, errno);
+      return std::string();
+    }
+    if (!S_ISLNK(sb.st_mode))
+    {
+      Dbg("%1% is not a symlink", selfPath);
+      return std::string();
+    }
+    
+    std::vector<char> filename(1024);
+    for (;;)
+    {
+      const int len = ::readlink(selfPath.c_str(), &filename[0], filename.size() - 1);
+      if (len == -1)
+      {
+        Dbg("Failed to readlink '%1%' (errno %2%)", selfPath, errno);
+        return std::string();
+      }
+      else if (len == static_cast<int>(filename.size()) - 1)
+      {
+        filename.resize(filename.size() * 2);
+      }
+      else
+      {
+        filename[len] = 0;
+        break;
+      }
+    }
+    const std::string result(&filename[0]);
+    Dbg("Executable name: %1%", result);
+    return result;
   }
 }
 
@@ -60,6 +90,7 @@ namespace Platform
     static const std::string soName = GetSharedLibraryName();
     if (!soName.empty())
     {
+      Dbg("Shared library name is '%1%'", soName);
       return soName;
     }
     static const std::string binName = GetExecutableName();
