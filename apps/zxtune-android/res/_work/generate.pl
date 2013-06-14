d4a3a247b61b2b3f24a513103822956dc5fd7876
vitamin-caig <vitamin.caig@gmail.com>
2013-06-14 23:10:57 +0000

    zxtune-android: add missed icons. Add icons generator.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@2476 e19a5227-2556-0410-816f-a35683127991

---
+++ apps/zxtune-android/res/_work/generate.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+my $traits = {
+              launcher => {'-ldpi' => '36x36', '-mdpi' => '48x48', '-hdpi' => '72x72', '-xhdpi' => '96x96'},
+              status =>   {'-ldpi' => '18x18', '-mdpi' => '24x24', '-hdpi' => '36x36', '-xhdpi' => '48x48'},
+              status_v8 => {'' => '25x25'},
+              status_v9 => {'-ldpi' => '12x19', '-mdpi' => '16x25', '-hdpi' => '24x38'},
+              listicon => {'-ldpi' => '24x24', '-mdpi' => '32x32', '-hdpi' => '48x48', '-xhdpi' => '64x64'}
+             };
+
+sub convert
+{
+  my $icon = shift or die 'No object';
+  $icon->{source} =~ /([^:]+)(:(.*))?/;
+  my $src = $1;
+  my $srcid = $3;
+  my $tgt = $icon->{target};
+  my $ctg = $icon->{category};
+  my $qual = $icon->{qualifiers};
+  for my $dpi ('', '-ldpi', '-mdpi', '-hdpi', '-xhdpi')
+  {
+    my $fld = "../drawable${dpi}${qual}";
+    next unless ${traits}->{${ctg}}->{${dpi}} =~ /(\d+)x(\d+)/;
+    my $w = $1;
+    my $h = $2;
+    mkdir ${fld} unless -d ${fld};
+    my $sz = ${w} >= ${h} ? ${h} : ${w};
+    my $cmd = "--without-gui --file=${src} --export-png=${fld}/${tgt} --export-width=${sz} --export-height=${sz} --export-area-page";
+    $cmd .= " --export-id=${srcid} --export-id-only" if ${srcid};
+    system("inkscape ${cmd}");
+    if (${h} > ${w})
+    {
+      system("convert ${fld}/${tgt} -bordercolor none -border 0x{${h}-${w}} -gravity center -crop ${w}x${h}+0+0 ${fld}/${tgt}");
+    }
+  }
+}
+
+convert({source => 'icon.svg', target => 'ic_launcher.png', category => 'launcher'});
+convert({source => 'status.svg:v11', target => 'ic_stat_notify_play.png', category => 'status', qualifiers => '-v11'});
+convert({source => 'status.svg:v9', target => 'ic_stat_notify_play.png', category => 'status_v9', qualifiers => '-v9'});
+convert({source => 'status.svg:v8', target => 'ic_stat_notify_play.png', category => 'status_v8', qualifiers => '-v8'});
+convert({source => 'folder.svg', target => 'ic_browser_folder.png', category => 'listicon'});
