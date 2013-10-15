6b9a4a8c326386b7b2128697a5f07a2e3f00dd39
vitamin-caig <vitamin.caig@gmail.com>
2013-10-15 07:50:34 +0000

    Fixes in Trush decoder.
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@2650 e19a5227-2556-0410-816f-a35683127991

--- src/formats/packed/trush.cpp
+++ src/formats/packed/trush.cpp
@@ -27,28 +27,49 @@ Author:
 //text includes
 #include <formats/text/packed.h>
 
-namespace TRUSH
+namespace Trush
 {
   const std::size_t MAX_DECODED_SIZE = 0xc000;
 
-  const std::string DEPACKER_PATTERN(
-    /*
-    "?"     //di/ei
-    "0610"  //ld b,0x10
-    "d9"    //exx
-    "e5"    //push hl
-    "21??"  //ld hl,xxxx
-    "11??"  //ld de,xxxx
-    "01??"  //ld bc,xxxx
-    //+0xe
-    "?{11}"
-    //+0x19
+  //Head and tail are delimited by optional signature (some versions store additional code there)
+
+  //At least two different prefixes
+  //using ix/iy
+  //Depacker beginning may be corrupted
+  const std::string DEPACKER_HEAD(
+    "???"   //di/ei      | jp xxxx
+            //ld b,0x10
+    "?"     //exx
+    "?"     //push hl
+    "???"   //ld hl,xxxx
+    "???"   //ld de,xxxx
+    "01??"  //ld bc,xxxx ;packed block size
+    "d5"    //push de
+    "ed(b0|b8)"//ldir/lddr
+    "eb"    //ex de,hl
+    "23"    //inc hl
+    "(dd|fd)21??" //ld ix/y,xxxxx
+    "(dd|fd)39"   //add ix/y,sp
     "f9"    //ld sp,hl
-    "21??"  //ld hl,xxxx
+    "21??"  //ld hl,xxxx ;addr of body
     "11??"  //ld de,xxxx
-    "01??"  //ld bc,xxxx
-    */
-    "?{75}"
+    "01??"  //ld bc,xxxx ;size of body
+    "d5"    //push de
+    "c3??"  //jp xxxx
+  );
+
+  const std::size_t HEAD_SIZE = 0x27;
+
+  //There's some screen compressor with the almost same depacker:
+  /*
+    +c0
+       ld sp,ix
+       ld hl,xxxx
+       exx
+       ei
+       ret
+  */
+  const std::string DEPACKER_BODY(
     "d9"    //exx
     "e1"    //pop hl
     "1806"  //jr xx
@@ -60,7 +81,27 @@ namespace TRUSH
     "d9"    //exx
     "29"    //add hl,hl
     "1003"  //djnz xx
+    "e1"    //pop hl
+    "0610"  //ld b,xx
+    /*
+    //+0x10
+    "?{176}"
+    //+0xc0
+    "?f9"   //ld sp,ix/y
+    "1b"     //dec de
+    "eb"     //ex de,hl
+    "d1"     //pop de
+    "01??"   //ld bc,xxxx
+    "ed?"    //lddr
+    //several variants
+    "?"      //pop hl | pop hl  | jp xxxx | di
+    "?"      //exx    | jp xxxx |         | jp xxxx
+    "?"      //ei/nop |         |
+    "?"      //ret    |         | nop
+    */
   );
+
+  const std::size_t BODY_SIZE = 0xce;
 
 #ifdef USE_PRAGMA_PACK
 #pragma pack(push,1)
@@ -72,56 +113,68 @@ namespace TRUSH
     //+c
     uint16_t SizeOfPacked;
     //+e
-    char Padding2[0x19];
-    //+27
-    char Signature[0x24];
-    //+4b
-    char Padding3[0xce];
-    //+119
-    uint8_t BitStream[2];
+    char Padding2[0x13];
+    //+21
+    uint16_t DepackerBodySize;
   } PACK_POST;
 #ifdef USE_PRAGMA_PACK
 #pragma pack(pop)
 #endif
 
-  BOOST_STATIC_ASSERT(sizeof(RawHeader) == 0x11b);
-
-  const std::size_t MIN_SIZE = sizeof(RawHeader);
+  const std::size_t MIN_DATA_SIZE = 2;
+  const std::size_t MAX_HEAD_SIZE = HEAD_SIZE + 36;//typical signature
+  const std::size_t MIN_BODY_SIZE = BODY_SIZE + MIN_DATA_SIZE;
+  const std::size_t MIN_SIZE = HEAD_SIZE + MIN_BODY_SIZE;
 
   class Container
   {
   public:
-    Container(const void* data, std::size_t size)
+    Container(const void* data, std::size_t size, std::size_t bodyOffset)
       : Data(static_cast<const uint8_t*>(data))
       , Size(size)
+      , DepackerSize(bodyOffset + GetBodySize())
     {
     }
 
     bool FastCheck() const
     {
-      if (Size <= sizeof(RawHeader))
+      if (Size <= DepackerSize + MIN_DATA_SIZE)
       {
         return false;
       }
-      const RawHeader& header = GetHeader();
       const std::size_t usedSize = GetUsedSize();
-      return Math::InRange(usedSize, sizeof(header), Size);
+      return Math::InRange(usedSize, DepackerSize, Size);
     }
 
     uint_t GetUsedSize() const
     {
-      const RawHeader& header = GetHeader();
-      return sizeof(header) + fromLE(header.SizeOfPacked) - sizeof(header.BitStream);
+      return DepackerSize + GetPackedSize();
     }
 
+    const uint8_t* GetPackedData() const
+    {
+      return Data + DepackerSize;
+    }
+
+    std::size_t GetPackedSize() const
+    {
+      return fromLE(GetHeader().SizeOfPacked);
+    }
+  private:
     const RawHeader& GetHeader() const
     {
       assert(Size >= sizeof(RawHeader));
       return *safe_ptr_cast<const RawHeader*>(Data);
     }
+
+    std::size_t GetBodySize() const
+    {
+      return fromLE(GetHeader().DepackerBodySize);
+    }
   private:
     const uint8_t* const Data;
     const std::size_t Size;
+    const std::size_t DepackerSize;
   };
 
   class DataDecoder
@@ -129,8 +182,7 @@ namespace TRUSH
   public:
     explicit DataDecoder(const Container& container)
       : IsValid(container.FastCheck())
-      , Header(container.GetHeader())
-      , Stream(Header.BitStream, fromLE(Header.SizeOfPacked))
+      , Stream(container.GetPackedData(), container.GetPackedSize())
       , Result(new Dump())
       , Decoded(*Result)
     {
@@ -149,9 +201,7 @@ namespace TRUSH
   private:
     bool DecodeData()
     {
-      const uint_t packedSize = fromLE(Header.SizeOfPacked);
-
-      Decoded.reserve(packedSize * 2);
+      Decoded.reserve(Stream.GetRestBytes() * 2);
 
       while (!Stream.Eof() && Decoded.size() < MAX_DECODED_SIZE)
       {
@@ -240,7 +290,6 @@ namespace TRUSH
     }
   private:
     bool IsValid;
-    const RawHeader& Header;
     Hrust1Bitstream Stream;
     std::auto_ptr<Dump> Result;
     Dump& Decoded;
@@ -252,11 +301,16 @@ namespace Formats
 {
   namespace Packed
   {
-    class TRUSHDecoder : public Decoder
+    class TrushDecoder : public Decoder
     {
     public:
-      TRUSHDecoder()
-        : Depacker(Binary::Format::Create(TRUSH::DEPACKER_PATTERN, TRUSH::MIN_SIZE))
+      TrushDecoder()
+        : DepackerBody(Binary::Format::Create(Trush::DEPACKER_BODY, Trush::MIN_BODY_SIZE))
+        , Depacker(Binary::CreateCompositeFormat(
+          Binary::Format::Create(Trush::DEPACKER_HEAD),
+          DepackerBody,
+          Trush::HEAD_SIZE,
+          Trush::MAX_HEAD_SIZE))
       {
       }
 
@@ -276,21 +330,22 @@ namespace Formats
         {
           return Container::Ptr();
         }
-        const TRUSH::Container container(rawData.Start(), rawData.Size());
+        const Trush::Container container(rawData.Start(), rawData.Size(), DepackerBody->NextMatchOffset(rawData));
         if (!container.FastCheck())
         {
           return Container::Ptr();
         }
-        TRUSH::DataDecoder decoder(container);
+        Trush::DataDecoder decoder(container);
         return CreatePackedContainer(decoder.GetResult(), container.GetUsedSize());
       }
     private:
+      const Binary::Format::Ptr DepackerBody;
       const Binary::Format::Ptr Depacker;
     };
 
     Decoder::Ptr CreateTRUSHDecoder()
     {
-      return boost::make_shared<TRUSHDecoder>();
+      return boost::make_shared<TrushDecoder>();
     }
   }
 }
