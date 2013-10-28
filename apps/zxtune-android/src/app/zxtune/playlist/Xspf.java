32a50a90d06639bcfc21f955822edb5b13a91351
vitamin-caig <vitamin.caig@gmail.com>
2013-10-28 21:08:05 +0000

    zxtune-android: initial support of xspf playlists playback (no properties, no recursive search).
    git-svn-id: https://zxtune.googlecode.com/svn/trunk@2689 e19a5227-2556-0410-816f-a35683127991

---
+++ apps/zxtune-android/src/app/zxtune/playlist/Xspf.java
@@ -0,0 +1,39 @@
+package app.zxtune.playlist;
+
+final class Xspf {
+  
+  final static String ENCODING = "UTF-8";
+  final static String XMLNS = "http://xspf.org/ns/0/";
+  final static String APPLICATION = "http://zxtune.googlecode.com";
+  final static String VERSION = "1";
+  
+  final static class Tags {
+
+    final static String PLAYLIST = "playlist";
+    final static String EXTENSION = "extension";
+
+    final static String PROPERTY = "property";
+    
+    final static String TRACKLIST = "trackList";
+    
+    final static String TRACK = "track";
+    final static String LOCATION = "location";
+    final static String CREATOR = "creator";
+    final static String TITLE = "title";
+    final static String DURATION = "duration";
+  }
+  
+  final static class Attributes {
+    final static String VERSION = "version";
+    final static String XMLNS = "xmlns";
+    final static String APPLICATION = "application";
+    final static String NAME = "name";
+  }
+  
+  final static class Properties {
+    final static String PLAYLIST_CREATOR = "zxtune.app.playlist.creator";
+    final static String PLAYLIST_NAME = "zxtune.app.playlist.name";
+    final static String PLAYLIST_VERSION = "zxtune.app.playlist.version";
+    final static String PLAYLIST_ITEMS = "zxtune.app.playlist.items";
+  }
+}
