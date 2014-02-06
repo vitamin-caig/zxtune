ce1ef0102491085a3799bd1b9f14eb2e6239532c
vitamin-caig <vitamin.caig@gmail.com>
2014-02-06 11:58:11 +0400

    Update boost build scripts for msvs2013 and mingw 4.8.1.

--- make/build/windows/boost/build64.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set INSTPATH=E:\Build\boost-1.49.0-vc80-x86_64
-mkdir %INSTPATH%
-..\build.bat %INSTPATH% msvc 64
--- make/build/windows/boost/build_mingw64.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set MINGW_ROOT_DIRECTORY=e:\build\MinGW
-ECHO %PATH% | FIND "%MINGW_ROOT_DIRECTORY" > NUL && GOTO noset
-set PATH=%MINGW_ROOT_DIRECTORY%\bin;%PATH%
-:noset
-set INSTPATH=E:\Build\boost-1.49.0-mingw-x86_64
-mkdir %INSTPATH%
-..\build.bat %INSTPATH% gcc 64
