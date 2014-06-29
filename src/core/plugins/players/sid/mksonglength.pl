35d614902ea069067e557cb981b4b8ccf2c05e03
vitamin-caig <vitamin.caig@gmail.com>
2014-06-29 13:15:16 +0400

    Reduce size of SID song lengths database: lookup by digest crc32.

--- src/core/plugins/players/sid/mksonglength.pl
+++ src/core/plugins/players/sid/mksonglength.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 #use strict;
+use String::CRC32;
 
 my %Hashes = ();
 
@@ -8,18 +9,23 @@ while (my $line = <>)
 {
   next unless $line =~ /([0-9a-f]{32})=(.*)/;
   my $hash = $1;
-  my @times = split(' ',$2);
-  $Hashes{${hash}} = \@times;
+  my $crc = sprintf("%08x", crc32($hash));
+  my @times = split(' ', $2);
+  if (exists $Hashes{$crc})
+  {
+    die('Duplicated hash crc32');
+  }
+  $Hashes{$crc} = {'hash' => $hash, 'times' => \@times};
 }
 
-foreach my $hashstr (sort keys %Hashes)
+foreach my $crc (sort keys %Hashes)
 {
-  my $hash = $hashstr;
-  $hash =~ s/([0-9a-f]{2})/0x$1,/g;
-  my $timesstr = $Hashes{$hashstr};
+  my $hash = $Hashes{$crc}{'hash'};
+  my $timesstr = $Hashes{$crc}{'times'};
   my @times = map {$_ ? $_ : 1} map {$_ =~ /([0-9]+):([0-9]+)/; 60 * $1 + $2;} @$timesstr;
+  print "//$hash\n";
   foreach my $time (@times)
   {
-    print "{{{$hash}},$time},\n";
+    printf("{0x%s, %u},\n", $crc, $time);
   }
 }
--- src/core/plugins/players/sid/songlengths.cpp
+++ src/core/plugins/players/sid/songlengths.cpp
@@ -12,27 +12,23 @@
 #include "songlengths.h"
 //common includes
 #include <contract.h>
-//std includes
-#include <cstring>
-#include <cctype>
+#include <crc.h>
+#include <pointers.h>
 //boost includes
-#include <boost/array.hpp>
 #include <boost/range/end.hpp>
 
 namespace Module
 {
 namespace Sid
 {
-  typedef boost::array<uint8_t, 16> MD5Hash;
-  
   struct SongEntry
   {
-    const MD5Hash Hash;
+    const uint32_t HashCrc32;
     const unsigned Seconds;
     
-    bool operator < (const MD5Hash& hash) const
+    bool operator < (uint32_t hashCrc32) const
     {
-      return Hash < hash;
+      return HashCrc32 < hashCrc32;
     }
   };
   
@@ -45,40 +41,15 @@ namespace Sid
 
   const Seconds DEFAULT_SONG_LENGTH(120);
 
-  uint8_t Hex2Bin(char val)
-  {
-    Require(std::isxdigit(val));
-    return std::isdigit(val)
-      ? val - '0'
-      : std::toupper(val) - 'A' + 10;
-  }
-
-  uint8_t Hex2Bin(char hi, char lo)
-  {
-    return Hex2Bin(hi) * 16 + Hex2Bin(lo);
-  }
-
-  MD5Hash HashFromString(const char* str)
-  {
-    MD5Hash result;
-    Require(std::strlen(str) == 2 * result.size());
-    for (MD5Hash::iterator it = result.begin(), lim = result.end(); it != lim; ++it)
-    {
-      *it = Hex2Bin(str[0], str[1]);
-      str += 2;
-    }
-    return result;
-  }
-
   TimeType GetSongLength(const char* md5digest, uint_t idx)
   {
-    const MD5Hash hash = HashFromString(md5digest);
+    const uint32_t hashCrc32 = Crc32(safe_ptr_cast<const uint8_t*>(md5digest), 32);
     const SongEntry* const end = boost::end(SONGS);
-    const SongEntry* const lower = std::lower_bound(SONGS, end, hash);
-    if (lower + idx < end && lower->Hash == hash)
+    const SongEntry* const lower = std::lower_bound(SONGS, end, hashCrc32);
+    if (lower + idx < end && lower->HashCrc32 == hashCrc32)
     {
       const SongEntry* const entry = lower + idx;
-      if (lower->Hash == entry->Hash)
+      if (lower->HashCrc32 == entry->HashCrc32)
       {
         return Seconds(entry->Seconds);
       }
