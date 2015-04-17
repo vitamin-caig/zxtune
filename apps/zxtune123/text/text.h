e4ace2cebb50fc470b3527d4c7ab5f32c2e5fbf3
vitamin-caig <vitamin.caig@gmail.com>
2015-04-17 17:39:50 +0300

    zxtune123: add support of multidevice plugins.

--- apps/zxtune123/text/text.h
+++ apps/zxtune123/text/text.h
@@ -83,6 +83,7 @@ extern const Char INFO_CAP_HARDWARE[];
 extern const Char INFO_CAP_HWVOLUME[];
 extern const Char INFO_CAP_MODULE[];
 extern const Char INFO_CAP_MOS6581[];
+extern const Char INFO_CAP_MULTI[];
 extern const Char INFO_CAP_MULTITRACK[];
 extern const Char INFO_CAP_PLAIN[];
 extern const Char INFO_CAP_SAA1099[];
