ea5a0e90a3ebd94ba09880cdb0b011e6c6d9b5f7
vitamin-caig <vitamin.caig@gmail.com>
2015-07-08 16:54:38 +0300

    Import LZMA SDK v15.05beta.

---
+++ 3rdparty/lzma/CPP/7zip/IProgress.h
@@ -0,0 +1,20 @@
+// IProgress.h
+
+#ifndef __IPROGRESS_H
+#define __IPROGRESS_H
+
+#include "../Common/MyTypes.h"
+#include "../Common/MyUnknown.h"
+
+#include "IDecl.h"
+
+#define INTERFACE_IProgress(x) \
+  STDMETHOD(SetTotal)(UInt64 total) x; \
+  STDMETHOD(SetCompleted)(const UInt64 *completeValue) x; \
+
+DECL_INTERFACE(IProgress, 0, 5)
+{
+  INTERFACE_IProgress(PURE)
+};
+
+#endif
