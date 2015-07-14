3aa136db89b08a6167eb6b6819faf250378c00b8
vitamin-caig <vitamin.caig@gmail.com>
2015-07-14 17:44:33 +0300

    Update boost build scripts.

--- make/build/linux/boost/build_armhf.sh
+++ make/build/linux/boost/build_armhf.sh
@@ -3,7 +3,7 @@ BOOST_VERSION=`sed -nr 's/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/p' index.html`
 echo "Building boost version ${BOOST_VERSION} for linux-armhf"
 PREFIX=/mnt/devel/zxtune/Build/boost-${BOOST_VERSION}-linux-armhf
 mkdir -p ${PREFIX}
-echo "using gcc : arm : arm-linux-gnueabihf-g++ ;" > tools/build/v2/user-config.jam
+echo "using gcc : arm : arm-linux-gnueabihf-g++ ;" > tools/build/src/user-config.jam
 LD_LIBRARY_PATH=/opt/lib32/lib:/opt/lib32/usr/lib \
 PATH=${TOOLCHAIN_PATH}/bin:${PATH} \
 ./bjam --prefix=${PREFIX} toolset=gcc-arm link=static threading=multi target-os=linux --layout=system \
