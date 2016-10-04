2a66bc479889c6aab343cfe045d6c8b92015337d
vitamin-caig <vitamin.caig@gmail.com>
2016-10-04 14:46:26 +0300

    c++11: simplify MakeFormattedError.

--- include/error_tools.h
+++ include/error_tools.h
@@ -17,38 +17,9 @@
 //library includes
 #include <strings/format.h>
 
-//! @brief Building error object with formatted text using up to 5 parameters
-template<class P1>
-inline Error MakeFormattedError(Error::LocationRef loc, const String& fmt,
-  const P1& p1)
+//! @brief Building error object with formatted text
+template<class S, class... P>
+Error MakeFormattedError(Error::LocationRef loc, S&& fmt, P&&... p)
 {
-  return Error(loc, Strings::Format(fmt, p1));
-}
-
-template<class P1, class P2>
-inline Error MakeFormattedError(Error::LocationRef loc, const String& fmt,
-  const P1& p1, const P2& p2)
-{
-  return Error(loc, Strings::Format(fmt, p1, p2));
-}
-
-template<class P1, class P2, class P3>
-inline Error MakeFormattedError(Error::LocationRef loc, const String& fmt,
-  const P1& p1, const P2& p2, const P3& p3)
-{
-  return Error(loc, Strings::Format(fmt, p1, p2, p3));
-}
-
-template<class P1, class P2, class P3, class P4>
-inline Error MakeFormattedError(Error::LocationRef loc, const String& fmt,
-  const P1& p1, const P2& p2, const P3& p3, const P4& p4)
-{
-  return Error(loc, Strings::Format(fmt, p1, p2, p3, p4));
-}
-
-template<class P1, class P2, class P3, class P4, class P5>
-inline Error MakeFormattedError(Error::LocationRef loc, const String& fmt,
-  const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5)
-{
-  return Error(loc, Strings::Format(fmt, p1, p2, p3, p4, p5));
+  return Error(loc, Strings::Format(std::forward<S>(fmt), std::forward<P>(p)...));
 }
