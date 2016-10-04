a5fb585e531ddad948407ff91553e6cbe007eb99
vitamin-caig <vitamin.caig@gmail.com>
2016-10-04 14:43:07 +0300

    c++11: simplify MakePtr functions.

--- include/make_ptr.h
+++ include/make_ptr.h
@@ -10,90 +10,17 @@
 
 #pragma once
 
-//boost includes
-#include <boost/static_assert.hpp>
-#include <boost/type_traits/is_polymorphic.hpp>
-#include <boost/type_traits/is_same.hpp>
-#include <boost/type_traits/remove_cv.hpp>
+//std includes
+#include <utility>
 
-namespace Detail
+template<class T, class... P>
+inline typename T::Ptr MakePtr(P&&... p)
 {
-  template<class Ptr, class ObjType>
-  inline Ptr MakePtr(ObjType* obj)
-  {
-    using namespace boost;
-    typedef typename Ptr::element_type PtrType;
-    BOOST_STATIC_ASSERT(is_same<typename remove_cv<PtrType>::type, ObjType>::value || is_polymorphic<PtrType>::value);
-    return Ptr(static_cast<PtrType*>(obj));
-  }
+  return typename T::Ptr(new T(std::forward<P>(p)...));
 }
 
-template<class T>
-inline typename T::Ptr MakePtr()
+template<class T, class... P>
+inline typename T::RWPtr MakeRWPtr(P&&... p)
 {
-  return Detail::MakePtr<typename T::Ptr>(new T());
-}
-
-template<class T>
-inline typename T::RWPtr MakeRWPtr()
-{
-  return Detail::MakePtr<typename T::RWPtr>(new T());
-}
-
-//TODO: use universal references and variadic templates for c++11
-template<class T, class P1>
-inline typename T::Ptr MakePtr(const P1& p1)
-{
-  return Detail::MakePtr<typename T::Ptr>(new T(p1));
-}
-
-template<class T, class P1>
-inline typename T::RWPtr MakeRWPtr(const P1& p1)
-{
-  return Detail::MakePtr<typename T::RWPtr>(new T(p1));
-}
-
-template<class T, class P1, class P2>
-inline typename T::Ptr MakePtr(const P1& p1,
-                        const P2& p2)
-{
-  return Detail::MakePtr<typename T::Ptr>(new T(p1, p2));
-}
-
-template<class T, class P1, class P2, class P3>
-inline typename T::Ptr MakePtr(const P1& p1,
-                        const P2& p2,
-                        const P3& p3)
-{
-  return Detail::MakePtr<typename T::Ptr>(new T(p1, p2, p3));
-}
-
-template<class T, class P1, class P2, class P3, class P4>
-inline typename T::Ptr MakePtr(const P1& p1,
-                        const P2& p2,
-                        const P3& p3,
-                        const P4& p4)
-{
-  return Detail::MakePtr<typename T::Ptr>(new T(p1, p2, p3, p4));
-}
-
-template<class T, class P1, class P2, class P3, class P4, class P5>
-inline typename T::Ptr MakePtr(const P1& p1,
-                        const P2& p2,
-                        const P3& p3,
-                        const P4& p4,
-                        const P5& p5)
-{
-  return Detail::MakePtr<typename T::Ptr>(new T(p1, p2, p3, p4, p5));
-}
-
-template<class T, class P1, class P2, class P3, class P4, class P5, class P6>
-inline typename T::Ptr MakePtr(const P1& p1,
-                        const P2& p2,
-                        const P3& p3,
-                        const P4& p4,
-                        const P5& p5,
-                        const P6& p6)
-{
-  return Detail::MakePtr<typename T::Ptr>(new T(p1, p2, p3, p4, p5, p6));
+  return typename T::RWPtr(new T(std::forward<P>(p)...));
 }
