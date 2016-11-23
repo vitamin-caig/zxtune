85a9a09d213e29b2df67fa2d1da20e0e9999c8fd
vitamin-caig <vitamin.caig@gmail.com>
2016-11-23 20:43:59 +0300

    Simplify MakePtr to erase derived class usage et all.

--- include/make_ptr.h
+++ include/make_ptr.h
@@ -16,11 +16,11 @@
 template<class T, class... P>
 inline typename T::Ptr MakePtr(P&&... p)
 {
-  return typename T::Ptr(new T(std::forward<P>(p)...));
+  return typename T::Ptr(static_cast<typename std::remove_const<typename T::Ptr::element_type>::type*>(new T(std::forward<P>(p)...)));
 }
 
 template<class T, class... P>
 inline typename T::RWPtr MakeRWPtr(P&&... p)
 {
-  return typename T::RWPtr(new T(std::forward<P>(p)...));
+  return typename T::RWPtr(static_cast<typename std::remove_const<typename T::RWPtr::element_type>::type*>(new T(std::forward<P>(p)...)));
 }
