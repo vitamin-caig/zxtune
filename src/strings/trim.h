1c95f89e1af29b7edd642627a313a81cc30f5b2a
vitamin-caig <vitamin.caig@gmail.com>
2017-02-25 22:19:59 +0300

    Add Strings::TrimSpaces.

---
+++ src/strings/trim.h
@@ -0,0 +1,19 @@
+/**
+*
+* @file
+*
+* @brief  String trimming
+*
+* @author vitamin.caig@gmail.com
+*
+**/
+
+#pragma once
+
+//common includes
+#include <types.h>
+
+namespace Strings
+{
+  StringView TrimSpaces(StringView str);
+}
