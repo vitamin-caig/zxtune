acfad3985dafeafdc150aea62c6180a7bf077ad6
vitamin-caig <vitamin.caig@gmail.com>
2017-04-18 01:36:30 +0300

    mgba: fix portable implementation of anonymousMemoryMap to reset allocated memory.

--- 3rdparty/mgba/src/platform/portable/memory.c
+++ 3rdparty/mgba/src/platform/portable/memory.c
@@ -6,7 +6,7 @@
 #include <mgba-util/memory.h>
 
 void* anonymousMemoryMap(size_t size) {
-        return malloc(size);
+        return calloc(1, size);
 }
 
 void mappedMemoryFree(void* memory, size_t size) {
