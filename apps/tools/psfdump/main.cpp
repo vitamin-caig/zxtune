6a6bbc62ea0a4baefe5702300e6770ed7c779570
vitamin-caig <vitamin.caig@gmail.com>
2017-05-15 20:24:12 +0300

    Extract psfdump tool.

--- apps/tools/psfdump/main.cpp
+++ apps/tools/psfdump/main.cpp
@@ -8,57 +8,126 @@
 *
 **/
 
-#include "../../utils.h"
+//common includes
 #include <make_ptr.h>
+//library includes
+#include <binary/data_builder.h>
 #include <binary/compression/zlib_container.h>
-#include <formats/chiptune/emulation/portablesoundformat.h>
+#include <formats/chiptune/emulation/gameboyadvancesoundformat.h>
 #include <formats/chiptune/emulation/playstationsoundformat.h>
 #include <formats/chiptune/emulation/playstation2soundformat.h>
+#include <formats/chiptune/emulation/portablesoundformat.h>
 #include <formats/chiptune/emulation/ultra64soundformat.h>
-#include <formats/chiptune/emulation/gameboyadvancesoundformat.h>
 #include <strings/format.h>
 #include <time/duration.h>
+//std includes
+#include <iomanip>
+#include <iostream>
+#include <fstream>
 #include <memory>
 
 namespace
 {
+  Binary::Container::Ptr OpenFile(const std::string& name)
+  {
+    std::ifstream stream(name.c_str(), std::ios::binary);
+    if (!stream)
+    {
+      throw std::runtime_error("Failed to open " + name);
+    }
+    stream.seekg(0, std::ios_base::end);
+    const std::size_t size = stream.tellg();
+    stream.seekg(0);
+    Binary::DataBuilder data(size);
+    stream.read(static_cast<char*>(data.Allocate(size)), size);
+    return data.CaptureResult();
+  }
+
+  void Write(uint_t level, const char* msg)
+  {
+    if (level)
+    {
+      std::cout << std::setw(level) << ' ';
+    }
+    std::cout << msg << std::endl;
+  }
+  
+  template<class... P>
+  void Write(uint_t level, const char* msg, P&&... params)
+  {
+    Write(level, Strings::Format(msg, params...).c_str());
+  }
+  
+  char ToHex(uint_t nib)
+  {
+    return nib > 9 ? 'a' + (nib - 10) : '0' + nib;
+  }
+  
+  char ToSym(uint_t val)
+  {
+    return val >= ' ' && val < 0x7f ? val : '.';
+  }
+  
+  void DumpHex(uint_t level, const void* data, std::size_t size)
+  {
+    const std::size_t LINE_SIZE = 16;
+    const auto DUMP_SIZE = std::min<std::size_t>(size, 256);
+    for (std::size_t offset = 0; offset < DUMP_SIZE; )
+    {
+      const auto in = static_cast<const uint8_t*>(data) + offset;
+      const auto toPrint = std::min(size - offset, LINE_SIZE);
+      std::string msg(5 + 2 + LINE_SIZE * 3 + 2 + LINE_SIZE, ' ');
+      msg[0] = ToHex((offset >> 12) & 15);
+      msg[1] = ToHex((offset >>  8) & 15);
+      msg[2] = ToHex((offset >>  4) & 15);
+      msg[3] = ToHex((offset >>  0) & 15);
+      msg[5] = '|';
+      msg[7 + LINE_SIZE * 3] = '|';
+      for (std::size_t idx = 0; idx < toPrint; ++idx)
+      {
+        msg[7 + idx * 3 + 0] = ToHex(in[idx] >> 4);
+        msg[7 + idx * 3 + 1] = ToHex(in[idx] & 15);
+        msg[7 + LINE_SIZE * 3 + 2 + idx] = ToSym(in[idx]);
+      }
+      Write(level, msg.c_str());
+      offset += toPrint;
+    }
+    if (DUMP_SIZE != size)
+    {
+      Write(level, "<truncated>");
+    }
+  }
+
   class SectionDumper
   {
   public:
     using Ptr = std::unique_ptr<const SectionDumper>;
     
-    virtual void DumpReserved(const Binary::Container& blob) const = 0;
-    virtual void DumpProgram(const Binary::Container& blob) const = 0;
-  };
-  
-  class SimpleDumper : public SectionDumper
-  {
-  public:
-    void DumpReserved(const Binary::Container& blob) const override
+    virtual void DumpReserved(const Binary::Container& blob) const
     {
-      std::cout << " Reserved area: " << blob.Size() << " bytes" << std::endl;
+      DumpHex(2, blob.Start(), blob.Size());
     }
-    
-    void DumpProgram(const Binary::Container& blob) const override
+
+    virtual void DumpProgram(const Binary::Container& blob) const
     {
-      std::cout << " Program area: " << blob.Size() << " bytes" << std::endl;
+      DumpHex(2, blob.Start(), blob.Size());
     }
   };
   
-  class PSF1Dumper : public SimpleDumper
+  class PSF1Dumper : public SectionDumper
   {
   public:
     void DumpProgram(const Binary::Container& blob) const override
     {
-      SimpleDumper::DumpProgram(blob);
       try
       {
+        Write(2, "PS-X EXE image:");
         PSXExeDumper delegate;
         Formats::Chiptune::PlaystationSoundFormat::ParsePSXExe(blob, delegate);
       }
       catch (const std::exception&)
       {
-        std::cout << "  Corrupted PS-X EXE" << std::endl;
+        Write(3, "Corrupted");
       }
     }
     
@@ -68,45 +137,45 @@ namespace
     public:
       void SetRegisters(uint32_t pc, uint32_t gp) override
       {
-        std::cout << Strings::Format("  PC=0x%08x GP=0x%08x", pc, gp) << std::endl;
+        Write(3, "Registers: PC=0x%1$08x GP=0x%2$08x", pc, gp);
       }
       
       void SetStackRegion(uint32_t head, uint32_t size) override
       {
-        std::cout << Strings::Format("  Stack %u bytes at 0x%08x", size, head) << std::endl;
+        Write(3, "Stack: %1% (0x%1$08x) bytes at 0x%2$08x", size, head);
       }
       
       void SetRegion(String region, uint_t fps) override
       {
-        std::cout << "  Region: " << region;
-        if (fps)
-        {
-           std::cout << " (" << fps << " fps)";
-        }
-        std::cout << std::endl;
+        Write(3, "Region: %1% (%2% fps)", region, fps);
       }
 
       void SetTextSection(uint32_t address, const Binary::Data& content)
       {
-        std::cout << Strings::Format("  Text section: %1% (0x%1$08x) bytes at 0x%2$08x", content.Size(), address) << std::endl;
+        Write(3, "Text section: %1% (0x%1$08x) bytes at 0x%2$08x", content.Size(), address);
+        DumpHex(4, content.Start(), content.Size());
       };
     };
   };
   
-  class PSF2Dumper : public SimpleDumper
+  class PSF2Dumper : public SectionDumper
   {
   public:
     void DumpReserved(const Binary::Container& blob) const override
     {
-      SimpleDumper::DumpReserved(blob);
       try
       {
+        Write(2, "PS2 VFS:");
         PSF2VFSDumper delegate;
         Formats::Chiptune::Playstation2SoundFormat::ParseVFS(blob, delegate);
+        if (const auto total = delegate.GetTotalSize())
+        {
+          Write(2, "Total: %1% bytes (%2%%% ratio)", total, 100.0f * blob.Size() / total);
+        }
       }
       catch (const std::exception&)
       {
-        std::cout << "  Corrupted PSF2 VFS" << std::endl;
+        Write(3, "Corrupted");
       }
     }
   private:
@@ -115,31 +184,39 @@ namespace
     public:
       void OnFile(String path, Binary::Container::Ptr content) override
       {
-        std::cout << "  " << path << " (" << (content ? content->Size() : std::size_t(0)) << " bytes)" << std::endl;
+        const auto fileSize = content ? content->Size() : std::size_t(0);
+        Write(3, "%1%: %2% bytes", path, fileSize);
+        if (fileSize)
+        {
+          DumpHex(4, content->Start(), fileSize);
+        }
+        TotalSize += fileSize;
       }
+      
+      std::size_t GetTotalSize() const
+      {
+        return TotalSize;
+      }
+    private:
+      std::size_t TotalSize = 0;
     };
   };
   
-  class USFDumper : public SimpleDumper
+  class USFDumper : public SectionDumper
   {
   public:
-    void DumpProgram(const Binary::Container& blob) const override
-    {
-      SimpleDumper::DumpProgram(blob);
-    }
-
     void DumpReserved(const Binary::Container& blob) const override
     {
-      SimpleDumper::DumpReserved(blob);
       try
       {
+        Write(2, "Ultra64 state:");
         USFStateDumper delegate;
         Formats::Chiptune::Ultra64SoundFormat::ParseSection(blob, delegate);
         delegate.Dump();
       }
       catch (const std::exception&)
       {
-        std::cout << "  Corrupted USF state" << std::endl;
+        Write(3, "Corrupted");
       }
     }
   private:
@@ -158,8 +235,8 @@ namespace
       
       void Dump() const
       {
-        std::cout << "  ROM: " << Rom.ToString() << std::endl;
-        std::cout << "  SaveState: " << SaveState.ToString() << std::endl;
+        Rom.Dump("ROM");
+        SaveState.Dump("SaveState");
       }
     private:
       struct Area
@@ -176,9 +253,12 @@ namespace
           End = std::max<uint_t>(End, offset + size);
         }
         
-        String ToString() const
+        void Dump(const char* tag) const
         {
-          return Strings::Format("%1% chunks with %2% bytes total (%3%%% covered)", ChunksCount, ChunksSize, 100.0 * ChunksSize / End);
+          if (ChunksCount)
+          {
+            Write(3, "%1%: %2% chunks with %3% bytes total (%4%%% covered)", tag, ChunksCount, ChunksSize, 100.0f * ChunksSize / End);
+          }
         }
       };
       Area Rom;
@@ -186,20 +266,20 @@ namespace
     };
   };
   
-  class GSFDumper : public SimpleDumper
+  class GSFDumper : public SectionDumper
   {
   public:
     void DumpProgram(const Binary::Container& blob) const override
     {
-      SimpleDumper::DumpProgram(blob);
       try
       {
+        Write(2, "GBA ROM:");
         GBARomDumper delegate;
         Formats::Chiptune::GameBoyAdvanceSoundFormat::ParseRom(blob, delegate);
       }
       catch (const std::exception&)
       {
-        std::cout << "  Corrupted GBA Rom" << std::endl;
+        Write(3, "Corrupted");
       }
     }
   private:
@@ -208,12 +288,13 @@ namespace
     public:
       void SetEntryPoint(uint32_t addr) override
       {
-        std::cout << Strings::Format("  EntryPoint=0x%08x", addr) << std::endl;
+        Write(3, "EntryPoint: 0x%1$08x", addr);
       }
       
       void SetRom(uint32_t addr, const Binary::Data& content) override
       {
-        std::cout << Strings::Format("  ROM: %1% (0x%1$08x) bytes at 0x%2$08x", content.Size(), addr) << std::endl;
+        Write(3, "ROM: %1% (0x%1$08x) bytes at 0x%2$08x", content.Size(), addr);
+        DumpHex(4, content.Start(), content.Size());
       }
     };
   };
@@ -223,84 +304,87 @@ namespace
   public:
     void SetVersion(uint_t ver) override
     {
-      std::cout << " Type: " << DecodeVersion(ver) << std::endl;
+      Write(1, "Type: %1% (id=0x%2$02x)", DecodeVersion(ver), ver);
       Dumper = CreateDumper(ver);
     }
 
     void SetReservedSection(Binary::Container::Ptr blob) override
     {
+      Write(1, "Reserved area: %1% bytes", blob->Size());
       Dumper->DumpReserved(*blob);
     }
     
     void SetPackedProgramSection(Binary::Container::Ptr blob) override
     {
+      const auto packedSize = blob->Size();
       const auto unpacked = Binary::Compression::Zlib::CreateDeferredDecompressContainer(std::move(blob));
+      Write(1, "Program area: %1% bytes (%2% packed, %3%%% ratio)", unpacked->Size(), packedSize, 100.0f * packedSize / unpacked->Size());
       Dumper->DumpProgram(*unpacked);
     }
     
     void SetTitle(String title) override
     {
-      std::cout << " Title: " << title << std::endl;
+      Write(1, "Title: %1%", title);
     }
     
     virtual void SetArtist(String artist) override
     {
-      std::cout << " Artist: " << artist << std::endl;
+      Write(1, "Artist: %1%", artist);
     }
     
     void SetGame(String game) override
     {
-      std::cout << " Game: " << game << std::endl;
+      Write(1, "Game: %1%", game);
     }
     
     void SetYear(String date) override
     {
-      std::cout << " Year: " << date << std::endl;
+      Write(1, "Year: %1%", date);
     }
     
     void SetGenre(String genre) override
     {
-      std::cout << " Genre: " << genre << std::endl;
+      Write(1, "Genre: %1%", genre);
     }
     
     void SetComment(String comment) override
     {
-      std::cout << " Comment: " << comment << std::endl;
+      Write(1, "Comment: %1%", comment);
     }
     
     void SetCopyright(String copyright) override
     {
-      std::cout << " Copyright: " << copyright << std::endl;
+      Write(1, "Copyright: %1%", copyright);
     }
     
     void SetDumper(String dumper) override
     {
-      std::cout << " Dumper: " << dumper << std::endl;
+      Write(1, "Dumper: %1%", dumper);
     }
     
     void SetLength(Time::Milliseconds duration) override
     {
-      std::cout << " Length: " << Time::MillisecondsDuration(duration.Get(), Time::Milliseconds(1)).ToString() << std::endl;
+      Write(1, "Length: %1%", Time::MillisecondsDuration(duration.Get(), Time::Milliseconds(1)).ToString());
     }
     
     void SetFade(Time::Milliseconds fade) override
     {
-      std::cout << " Fade: " << Time::MillisecondsDuration(fade.Get(), Time::Milliseconds(1)).ToString() << std::endl;
+      Write(1, "Fade: %1%", Time::MillisecondsDuration(fade.Get(), Time::Milliseconds(1)).ToString());
     }
     
     void SetVolume(float vol) override
     {
-      std::cout << " Volume: " << vol << std::endl;
+      Write(1, "Volume: %1%", vol);
     }
     
     void SetTag(String name, String value) override
     {
-      std::cout << " " << name << ": " << value << std::endl;
+      Write(1, "%1%: %2%", name, value);
     }
 
     void SetLibrary(uint_t num, String filename) override
     {
-      std::cout << " Library #" << num << ": " << filename << std::endl;
+      Write(1, "Library #%1%: %2%", num, filename);
     }
     
   private:
@@ -324,10 +408,14 @@ namespace
         return "GameBoy Advance (GSF)";
       case 0x23: 
         return "Super NES (SNSF)";
+      case 0x24:
+        return "Nintendo DS (2SF)";
+      case 0x25:
+        return "Nitro Composer (NCSF)";
       case 0x41:
         return "Capcom QSound (QSF)";
       default:
-        return Strings::Format("Unknown type (%u)", ver);
+        return "Unknown";
       }
     }
     
@@ -344,29 +432,13 @@ namespace
       case Formats::Chiptune::GameBoyAdvanceSoundFormat::VERSION_ID:
         return MakePtr<GSFDumper>();
       default:
-        return MakePtr<SimpleDumper>();
+        return MakePtr<SectionDumper>();
       }
     }
 
   private:
     SectionDumper::Ptr Dumper;
   };
-  
-  /*
-  Binary::Data::Ptr Reparse(const Binary::Container& data)
-  {
-    try
-    {
-      const auto builder = Formats::Chiptune::PortableSoundFormat::CreateBlobBuilder();
-      Formats::Chiptune::PortableSoundFormat::Parse(data, *builder);
-      return builder->GetResult();
-    }
-    catch (const std::exception&)
-    {
-      return Binary::Data::Ptr();
-    }
-  }
-  */
 }
 
 int main(int argc, char* argv[])
@@ -380,26 +452,17 @@ int main(int argc, char* argv[])
     for (int arg = 1; arg < argc; ++arg)
     {
       const std::string filename(argv[arg]);
-      std::cout << filename << ':' << std::endl;
-      std::unique_ptr<Dump> rawData(new Dump());
-      Test::OpenFile(filename, *rawData);
-      const Binary::Container::Ptr data = Binary::CreateContainer(std::move(rawData));
+      Write(0, "%1%:", filename);
+      const auto data = OpenFile(filename);
       PSFDumper builder;
       if (const auto container = Formats::Chiptune::PortableSoundFormat::Parse(*data, builder))
       {
-        std::cout << "Done. Processed " << container->Size() << " bytes" << std::endl;
+        Write(1, "Size: %1% bytes (file %2% bytes, %3%%% used)", container->Size(), data->Size(), 100.0f * container->Size() / data->Size());
       }
       else
       {
-        std::cout << "Failed to parse" << std::endl;
+        Write(1, "Invalid format");
       }
-      /*
-      if (const auto recreated = Reparse(*data))
-      {
-        std::ofstream out(filename + ".zxtune", std::ios::binary);
-        out.write(static_cast<const char*>(recreated->Start()), recreated->Size());
-      }
-      */
     }
   }
   catch (const std::exception& e)
