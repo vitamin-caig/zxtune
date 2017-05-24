904562d3f787fcb3dfae046de0df9dcf50317781
vitamin-caig <vitamin.caig@gmail.com>
2017-05-24 21:12:59 +0300

    zxtune-qt: add vio2sf credits.

--- l10n/ru/ui_common.ts
+++ l10n/ru/ui_common.ts
@@ -40,6 +40,7 @@ Gfx: &lt;b&gt;Restorer&lt;/b&gt;&lt;br/&gt;
 &lt;li&gt;Used &lt;a href=&quot;https://github.com/kode54/Highly_Experimental/&quot;&gt;Highly Experimental&lt;/a&gt; library by &lt;b&gt;Neill Corlett&lt;/b&gt;&lt;/li&gt;
 &lt;li&gt;Used &lt;a href=&quot;https://bitbucket.org/kode54/lazyusf2/&quot;&gt;lazyusf2&lt;/a&gt; library by &lt;b&gt;Christopher Snowhill&lt;/b&gt;&lt;/li&gt;
 &lt;li&gt;Used &lt;a href=&quot;http://mgba.io/&quot;&gt;mGBA&lt;/a&gt; sources by &lt;b&gt;Jeffrey Pfau&lt;/b&gt;&lt;/li&gt;
+&lt;li&gt;Used &lt;a href=&quot;https://gitlab.kode54.net/kode54/vio2sf&quot;&gt;vio2sf&lt;/a&gt; library by &lt;b&gt;Christopher Snowhill&lt;/b&gt;&lt;/li&gt;
 &lt;/ul&gt;
 
 &lt;center&gt;Dedicated to &lt;b&gt;Lizzi&lt;/b&gt;&lt;/center&gt;
@@ -74,6 +75,7 @@ Gfx: &lt;b&gt;Restorer&lt;/b&gt;&lt;br/&gt;
 &lt;li&gt;Использована библиотека &lt;a href=&quot;https://github.com/kode54/Highly_Experimental/&quot;&gt;Highly Experimental&lt;/a&gt; от &lt;b&gt;Neill Corlett&lt;/b&gt;&lt;/li&gt;
 &lt;li&gt;Использована библиотека &lt;a href=&quot;https://bitbucket.org/kode54/lazyusf2/&quot;&gt;lazyusf2&lt;/a&gt; от &lt;b&gt;Christopher Snowhill&lt;/b&gt;&lt;/li&gt;
 &lt;li&gt;Использованы исходные коды &lt;a href=&quot;http://mgba.io/&quot;&gt;mGBA&lt;/a&gt; от &lt;b&gt;Jeffrey Pfau&lt;/b&gt;&lt;/li&gt;
+&lt;li&gt;Использована библиотека &lt;a href=&quot;https://gitlab.kode54.net/kode54/vio2sf&quot;&gt;vio2sf&lt;/a&gt; от &lt;b&gt;Christopher Snowhill&lt;/b&gt;&lt;/li&gt;
 &lt;/ul&gt;
 
 &lt;center&gt;Посвящается &lt;b&gt;Lizzi&lt;/b&gt;&lt;/center&gt;
