69928c2b837dfeee197ab250a4c33136d8187a1d
vitamin-caig <vitamin.caig@gmail.com>
2017-10-03 04:14:02 +0300

    zxtune-android: disable debug logs for non-debug builds.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/Log.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/Log.java
@@ -20,11 +20,15 @@ public final class Log {
   
   // public interface
   public static void d(String tag, String msg) {
-    android.util.Log.d(tag, msg);
+    if (BuildConfig.DEBUG) {
+      android.util.Log.d(tag, msg);
+    }
   }
 
   public static void d(String tag, String msg, Object... params) {
-    d(tag, String.format(Locale.US, msg, params));
+    if (BuildConfig.DEBUG) {
+      android.util.Log.d(tag, String.format(Locale.US, msg, params));
+    }
   }
 
   public static void w(String tag, Throwable e, String msg) {
