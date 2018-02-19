c38350c7caa24678c504f9486e6816b64b25c858
vitamin-caig <vitamin.caig@gmail.com>
2018-02-20 01:05:57 +0300

    zxtune-android: fix NPE in Identifier.parse().

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/Identifier.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/Identifier.java
@@ -1,11 +1,11 @@
 /**
- * 
+ *
  * @file
  *
  * @brief Module identifier helper
  *
  * @author vitamin.caig@gmail.com
- * 
+ *
  */
 
 package app.zxtune;
@@ -14,19 +14,19 @@ import android.net.Uri;
 import android.text.TextUtils;
 
 public class Identifier {
-  
+
   private static final String SUBPATH_DELIMITER = "/";
   public static final Identifier EMPTY = new Identifier(Uri.EMPTY);
 
   /**
    * Identifier is not fully compatible with playlists from desktop version of zxtune
-   * 
+   *
    * E.g. first track in ay file is stored as test.ay?#1 without hash sign encoding
    */
   private final Uri fullpath;
   private final Uri location;
   private final String subpath;
-  
+
   public Identifier(Uri location, String subpath) {
     this.fullpath = withSubpath(location, subpath);
     this.location = location;
@@ -38,22 +38,24 @@ public class Identifier {
     this.location = withSubpath(fullpath, "");
     this.subpath = getSubpath(fullpath);
   }
-  
+
   public static Identifier parse(String str) {
-    return new Identifier(Uri.parse(str));
+    return str != null
+            ? new Identifier(Uri.parse(str))
+        : EMPTY;
   }
-  
+
   private static Uri withSubpath(Uri in, String subpath) {
     final Uri.Builder builder = in.buildUpon();
     builder.fragment(subpath);
     return builder.build();
   }
-  
+
   private static String getSubpath(Uri in) {
     final String fragment = in.getFragment();
     return fragment != null ? fragment : "";
   }
-  
+
   public final Uri getFullLocation() {
     return fullpath;
   }
@@ -61,43 +63,43 @@ public class Identifier {
   public final Uri getDataLocation() {
     return location;
   }
-  
+
   public final String getSubpath() {
     return subpath;
   }
-  
+
   private Identifier withSubpath(String subpath) {
     return new Identifier(location, subpath);
   }
-  
+
   public final Identifier withSubpath(String[] components) {
     return withSubpath(TextUtils.join(SUBPATH_DELIMITER, components));
   }
-  
+
   public final String getDisplayFilename() {
     final String filename = location.getLastPathSegment();
     if (subpath.length() == 0) {
       return filename;
     } else {
       final String subname = subpath.substring(subpath.lastIndexOf(SUBPATH_DELIMITER) + 1);
-      return filename + " > " + subname;  
+      return filename + " > " + subname;
     }
   }
-  
+
   public final String[] getSubpathComponents() {
     return TextUtils.split(subpath, SUBPATH_DELIMITER);
   }
-  
+
   @Override
   public String toString() {
     return fullpath.toString();
   }
-  
+
   @Override
   public int hashCode() {
     return fullpath.hashCode();
   }
-  
+
   @Override
   public boolean equals(Object rh) {
     if (rh instanceof Identifier) {
