088c11ee5ea9ec801ba0b5a5e3325df042c9a61a
vitamin-caig <vitamin.caig@gmail.com>
2018-04-24 03:04:54 +0300

    zxtune-android: use generic http catalogues support for joshw. Unescape filenames, change cache folder name.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/VfsRootJoshw.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/VfsRootJoshw.java
@@ -6,43 +6,28 @@
 
 package app.zxtune.fs;
 
-/**
- * Paths:
- * <p/>
- * 1) joshw:/${Catalogue}/${FilePath} - direct access to files or folders
- */
-
 import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
-import android.text.format.Formatter;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TreeSet;
 
 import app.zxtune.R;
 import app.zxtune.fs.cache.CacheDir;
-import app.zxtune.fs.joshw.Catalog;
+import app.zxtune.fs.httpdir.Catalog;
+import app.zxtune.fs.httpdir.HttpRootBase;
+import app.zxtune.fs.joshw.Path;
 
-final class VfsRootJoshw extends StubObject implements VfsRoot {
-
-  private static final String TAG = VfsRootJoshw.class.getName();
-
-  private static final String SCHEME = "joshw";
-
-  private static final int POS_BASE_ID = 0;
+final class VfsRootJoshw extends HttpRootBase implements VfsRoot {
 
   private final Context context;
-  private final Catalog catalog;
   private final AudiobaseDir bases[];
 
-  VfsRootJoshw(Context context, HttpProvider http, CacheDir cache) {
+  VfsRootJoshw(Context context, HttpProvider http, CacheDir cache) throws IOException {
+    super(Catalog.create(context, http, cache, "joshw"), Path.create());
     this.context = context;
-    this.catalog = Catalog.create(http, cache);
     this.bases = new AudiobaseDir[]{
             new AudiobaseDir("gbs", R.string.vfs_joshw_gbs_name, R.string.vfs_joshw_gbs_description),
             new AudiobaseDir("hes", R.string.vfs_joshw_hes_name, R.string.vfs_joshw_hes_description),
@@ -60,11 +45,6 @@ final class VfsRootJoshw extends StubObject implements VfsRoot {
   }
 
   @Override
-  public Uri getUri() {
-    return rootUri().build();
-  }
-
-  @Override
   public String getName() {
     return context.getString(R.string.vfs_joshw_root_name);
   }
@@ -72,12 +52,6 @@ final class VfsRootJoshw extends StubObject implements VfsRoot {
   @Override
   public String getDescription() {
     return context.getString(R.string.vfs_joshw_root_description);
-  }
-
-  @Override
-  @Nullable
-  public VfsObject getParent() {
-    return null;
   }
 
   @Override
@@ -99,47 +73,44 @@ final class VfsRootJoshw extends StubObject implements VfsRoot {
   @Override
   @Nullable
   public VfsObject resolve(Uri uri) throws IOException {
-    if (SCHEME.equals(uri.getScheme())) {
-      return resolvePath(uri);
+    return resolve(Path.parse(uri));
+  }
+
+  @Override
+  @Nullable
+  public VfsDir makeDir(app.zxtune.fs.httpdir.Path path) {
+    final Path ownPath = (Path) path;
+    return ownPath.isCatalogue()
+            ? resolveCatalogue(ownPath.getCatalogue())
+            : super.makeDir(path);
+  }
+
+  @Nullable
+  private VfsDir resolveCatalogue(String id) {
+    for (AudiobaseDir dir : bases) {
+      if (id.equals(dir.getBaseId())) {
+        return dir;
+      }
     }
     return null;
   }
 
-  private static Uri.Builder rootUri() {
-    return new Uri.Builder().scheme(SCHEME);
+  @Override
+  public VfsFile makeFile(app.zxtune.fs.httpdir.Path path, String size) {
+    return new MusicFile(path, size);
   }
 
-  @Nullable
-  private VfsObject resolvePath(Uri uri) throws IOException {
-    final List<String> path = uri.getPathSegments();
-    if (path.isEmpty()) {
-      return this;
-    } else {
-      final String id = path.get(POS_BASE_ID);
-      for (AudiobaseDir dir : bases) {
-        if (id.equals(dir.getBaseId())) {
-          return dir.resolve(path);
-        }
-      }
-      return null;
-    }
-  }
-
-  private class AudiobaseDir extends StubObject implements VfsDir {
+  private class AudiobaseDir extends HttpDir {
 
     private final String id;
     private final int name;
     private final int description;
 
     AudiobaseDir(String id, int nameRes, int descRes) {
+      super(rootPath.getChild(id));
       this.id = id;
       this.name = nameRes;
       this.description = descRes;
-    }
-
-    @Override
-    public Uri getUri() {
-      return rootUri().appendPath(getBaseId()).build();
     }
 
     @Override
@@ -157,201 +128,60 @@ final class VfsRootJoshw extends StubObject implements VfsRoot {
       return VfsRootJoshw.this;
     }
 
-    @Override
-    public void enumerate(Visitor visitor) throws IOException {
-      new MusicSubdir(id).enumerate(visitor);
-    }
-
-    public String getBaseId() {
+    final String getBaseId() {
       return id;
     }
+  }
 
-    public VfsObject resolve(List<String> path) throws IOException {
-      final int lastPathComponent = path.size() - 1;
-      if (POS_BASE_ID == lastPathComponent) {
-        // joshw://xxx
-        return this;
-      } else {
-        // joshw://xxx/yyy
-        final ByteBuffer content = catalog.getFileContent(path);
-        return catalog.isDirContent(content)
-                ? new MusicSubdir(path, content)
-                : new MusicFile(path, content);
-      }
+  private class MusicFile extends HttpFile {
+
+    //${title} (\[${alt title}\])?(\(${tag}\))*
+    private String name;
+    private String description;
+
+    MusicFile(app.zxtune.fs.httpdir.Path path, String size) {
+      super(path, size);
+      loadMetainfo();
     }
 
-    private abstract class MusicObject extends StubObject implements VfsObject {
-
-      final List<String> path;
-      ByteBuffer content;
-
-      MusicObject(String path) {
-        this.path = new ArrayList<>();
-        this.path.add(path);
-      }
-
-      MusicObject(List<String> path) {
-        this.path = path;
-      }
-
-      MusicObject(List<String> path, ByteBuffer content) {
-        this.path = path;
-        this.content = content;
-      }
-
-      @Override
-      public Uri getUri() {
-        final Uri.Builder builder = rootUri();
-        for (String dir : path) {
-          builder.appendPath(dir);
-        }
-        return builder.build();
-      }
-
-      @Override
-      public VfsObject getParent() {
-        final int parentDepth = path.size() - 1;
-        return parentDepth == 1
-                ? AudiobaseDir.this//root folder
-                : new MusicSubdir(path.subList(0, parentDepth));
-      }
-
-      ByteBuffer getContent() throws IOException {
-        if (content == null) {
-          content = catalog.getFileContent(path);
-        }
-        return content;
-      }
-    }
-
-    private class MusicSubdir extends MusicObject implements VfsDir {
-
-      MusicSubdir(String path) {
-        super(path);
-      }
-
-      MusicSubdir(List<String> path) {
-        super(path);
-      }
-
-      MusicSubdir(List<String> path, ByteBuffer content) {
-        super(path, content);
-      }
-
-      @Override
-      public String getName() {
-        return Uri.decode(path.get(path.size() - 1));
-      }
-
-      @Override
-      public void enumerate(final Visitor visitor) throws IOException {
-        final ByteBuffer content = getContent();
-        visitor.onItemsCount(getApproxItemsCount(content.limit()));
-        catalog.parseDir(content, new Catalog.DirVisitor() {
-          @Override
-          public void acceptDir(String name) {
-            visitor.onDir(makeNestedDir(name));
-          }
-
-          @Override
-          public void acceptFile(String name, String size) {
-            visitor.onFile(makeNestedFile(name, size));
-          }
-        });
-      }
-
-      private int getApproxItemsCount(int size) {
-        /*
-         *  at gbs.joshw.info
-         *    0-9 with 6 entries is 2777 bytes
-         *    s with 168 entries is 55251 bytes
-         *  ~324 bytes per entry, ~835 bytes constant part
-         */
-        final int ENTRY_SIZE = 324;
-        final int AUX_SIZE = 835;
-        return size > AUX_SIZE ? (size - AUX_SIZE) / ENTRY_SIZE : 0;
-      }
-
-      private VfsDir makeNestedDir(String name) {
-        final ArrayList<String> nestedPath = createNestedPath(name);
-        return new MusicSubdir(nestedPath);
-      }
-
-      private VfsFile makeNestedFile(String name, String size) {
-        final ArrayList<String> nestedPath = createNestedPath(name);
-        return new MusicFile(nestedPath, size);
-      }
-
-      private ArrayList<String> createNestedPath(String name) {
-        final ArrayList<String> result = new ArrayList<>(path.size() + 1);
-        result.addAll(path);
-        result.add(name);
-        return result;
-      }
-    }
-
-    private class MusicFile extends MusicObject implements VfsFile {
-
-      //${title} (\[${alt title}\])?(\(${tag}\))*
-      private String name;
-      private String description;
-      private final String size;
-
-      MusicFile(List<String> path, String size) {
-        super(path);
+    @Override
+    public String getName() {
+      if (name == null) {
         loadMetainfo();
-        this.size = size;
       }
+      return name;
+    }
 
-      MusicFile(List<String> path, ByteBuffer content) {
-        super(path, content);
+    @Override
+    public String getDescription() {
+      if (description == null) {
         loadMetainfo();
-        this.size = Formatter.formatShortFileSize(context, content.limit());
       }
+      return description;
+    }
 
-      @Override
-      public String getName() {
-        return name;
+    private void loadMetainfo() {
+      final String filename = path.getName();
+      int endOfName = filename.indexOf('(');
+      if (endOfName == -1) {
+        endOfName = filename.lastIndexOf('.');
       }
-
-      @Override
-      public String getDescription() {
-        return description;
-      }
-
-      @Override
-      public String getSize() {
-        return size;
-      }
-
-      @Override
-      public ByteBuffer getContent() throws IOException {
-        return super.getContent();
-      }
-
-      private void loadMetainfo() {
-        final String filename = Uri.decode(path.get(path.size() - 1));
-        int endOfName = filename.indexOf('(');
-        if (endOfName == -1) {
-          endOfName = filename.lastIndexOf('.');
+      name = filename.substring(0, endOfName - 1).trim();
+      final TreeSet<String> meta = new TreeSet<>();
+      for (int prevMetaPos = endOfName; ; ) {
+        final int metaPos = filename.indexOf('(', prevMetaPos);
+        if (metaPos == -1) {
+          break;
         }
-        name = filename.substring(0, endOfName - 1).trim();
-        final TreeSet<String> meta = new TreeSet<>();
-        for (int prevMetaPos = endOfName; ; ) {
-          final int metaPos = filename.indexOf('(', prevMetaPos);
-          if (metaPos == -1) {
-            break;
-          }
-          final int metaPosEnd = filename.indexOf(')', metaPos);
-          if (metaPosEnd == -1) {
-            break;
-          }
-          final String field = filename.substring(metaPos + 1, metaPosEnd);
-          meta.add(field);
-          prevMetaPos = metaPosEnd;
+        final int metaPosEnd = filename.indexOf(')', metaPos);
+        if (metaPosEnd == -1) {
+          break;
         }
-        description = TextUtils.join(" - ", meta);
+        final String field = filename.substring(metaPos + 1, metaPosEnd);
+        meta.add(field);
+        prevMetaPos = metaPosEnd;
       }
+      description = TextUtils.join(" - ", meta);
     }
   }
 }
---
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/joshw/Path.java
@@ -0,0 +1,137 @@
+package app.zxtune.fs.joshw;
+
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+/*
+ * joshw:/${Catalogue}/${FilePath}
+ */
+public final class Path implements app.zxtune.fs.httpdir.Path {
+
+  private static final String SCHEME = "joshw";
+
+  private static final String STORAGE_MIRROR = "http://%s.joshw.info/%s";
+
+  private final String catalogue;
+  private final List<String> elements;
+
+  private Path(String catalogue, List<String> elements) {
+    this.catalogue = catalogue;
+    this.elements = elements;
+  }
+
+  @Override
+  public String getRemoteUrl() {
+    return String.format(Locale.US, STORAGE_MIRROR, catalogue, TextUtils.join("/", elements));
+  }
+
+  @Override
+  public String getLocalId() {
+    return catalogue + "/" + TextUtils.join("/", elements);
+  }
+
+  @Override
+  @Nullable
+  public Path getParent() {
+    final int count = elements.size();
+    switch (count) {
+      case 0:
+        return TextUtils.isEmpty(catalogue) ? null : new Path(null, Collections.EMPTY_LIST);
+      case 1:
+        return new Path(catalogue, Collections.EMPTY_LIST);
+      default:
+        return new Path(catalogue, elements.subList(0, count - 1));
+    }
+  }
+
+  @Override
+  public Uri getUri() {
+    final Uri.Builder builder = new Uri.Builder().scheme(SCHEME);
+    if (!TextUtils.isEmpty(catalogue)) {
+      builder.appendPath(catalogue);
+      for (String el : elements) {
+        builder.appendPath(el);
+      }
+    }
+    return builder.build();
+  }
+
+  @Override
+  @Nullable
+  public final String getName() {
+    final int count = elements.size();
+    return count > 0
+            ? elements.get(count - 1)
+            : catalogue;
+  }
+
+  @Override
+  public Path getChild(String name) {
+    if (isEmpty()) {
+      return new Path(name, Collections.EMPTY_LIST);
+    } else {
+      final ArrayList<String> result = new ArrayList<>(elements.size() + 1);
+      result.addAll(elements);
+      result.add(name);
+      return new Path(catalogue, result);
+    }
+  }
+
+  @Override
+  public final boolean isEmpty() {
+    return TextUtils.isEmpty(catalogue);
+  }
+
+  @Override
+  public final boolean isFile() {
+    return isFileName(getName());
+  }
+
+  private static boolean isFileName(@Nullable String component) {
+    return component != null && component.toLowerCase().endsWith(".7z");
+  }
+
+  public final boolean isCatalogue() {
+    return elements.isEmpty();
+  }
+
+  public final String getCatalogue() {
+    return catalogue;
+  }
+
+  @Nullable
+  public static Path parse(Uri uri) {
+    if (SCHEME.equals(uri.getScheme())) {
+      final List<String> elements = uri.getPathSegments();
+      final int count = elements.size();
+      switch (count) {
+        case 0:
+          return new Path(null, Collections.EMPTY_LIST);
+        case 1:
+          return new Path(elements.get(0), Collections.EMPTY_LIST);
+        default:
+          final String catalogue = elements.get(0);
+          //compatibility
+          final ArrayList<String> decoded = new ArrayList<>(count - 1);
+          for (int idx = 1; idx < count; ++idx) {
+            decoded.add(Uri.decode(elements.get(idx)));
+          }
+          return new Path(catalogue, decoded);
+      }
+    } else {
+      return null;
+    }
+  }
+
+  public static Path create() {
+    return new Path(null, Collections.EMPTY_LIST);
+  }
+}
