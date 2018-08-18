39ee311bd1a01a92887e68111f2c7f42643aaeb5
vitamin-caig <vitamin.caig@gmail.com>
2018-08-18 04:11:06 +0300

    zxtune-android: support httpdir catalogues based on nginx xml autoindex format.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/httpdir/RemoteCatalog.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/httpdir/RemoteCatalog.java
@@ -2,7 +2,7 @@ package app.zxtune.fs.httpdir;
 
 import android.net.Uri;
 import android.support.annotation.NonNull;
-
+import app.zxtune.fs.http.HttpProvider;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,13 +11,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
-
-import app.zxtune.fs.http.HttpProvider;
+import java.util.Locale;
 
 final class RemoteCatalog extends Catalog {
-
-  private static final byte[] HTML_SIGNATURE = {'<', '!', 'D', 'O', 'C', 'T', 'Y', 'P', 'E'};
 
   private final HttpProvider http;
 
@@ -38,25 +34,47 @@ final class RemoteCatalog extends Catalog {
   @Override
   public void parseDir(Path path, DirVisitor visitor) throws IOException {
     final ByteBuffer data = getFileContent(path);
-    if (!isDirContent(data)) {
-      throw new IOException("Not a directory file");
-    }
     parseDir(data, visitor);
-  }
-
-  private static boolean isDirContent(ByteBuffer buf) {
-    final byte[] head = new byte[HTML_SIGNATURE.length];
-    final int readBytes = buf.get(head).position();
-    buf.position(0);
-    return readBytes == head.length && Arrays.equals(head, HTML_SIGNATURE);
   }
 
   private void parseDir(ByteBuffer data, DirVisitor visitor) throws IOException {
     final Document doc = Jsoup.parse(new ByteArrayInputStream(data.array()), null, "");
-    if (parseTableMarkup(doc, visitor) || parsePreMarkup(doc, visitor)) {
+    if (parseXmlIndex(doc, visitor) || parseTableMarkup(doc, visitor) || parsePreMarkup(doc, visitor)) {
       return;
     }
     throw new IOException("Unsupported format of html page");
+  }
+
+  /*
+  <list>
+    <file mtime="YYYY-MM-DDThh:mm:ssZ" size="bytes">name</file>
+    <directory mtime="YYYY-MM-DDThh:mm:ssZ">name</directory>
+  </list>
+   */
+  private boolean parseXmlIndex(Document doc, DirVisitor visitor) {
+    final Element list = doc.selectFirst("list");
+    if (list == null) {
+      return false;
+    }
+    for (Element row : list.select("directory")) {
+      final String name = row.text();
+      visitor.acceptDir(name);
+    }
+    for (Element row : list.select("file")) {
+      final String name = row.text();
+      final String sizeInBytes = row.attr("size");
+      visitor.acceptFile(name, formatSize(Integer.parseInt(sizeInBytes)));
+    }
+    return true;
+  }
+
+  private static String formatSize(int v) {
+    if (v < 1024) {
+      return Integer.toString(v);
+    } else {
+      final int z = (31 - Integer.numberOfLeadingZeros(v)) / 10;
+      return String.format(Locale.US, "%.1f%s", (float) v / (1 << (z * 10)), " KMG".charAt(z));
+    }
   }
 
   /*
@@ -79,7 +97,7 @@ final class RemoteCatalog extends Catalog {
          ...
        </table>
    */
-  private boolean parseTableMarkup(Document doc, DirVisitor visitor) throws IOException {
+  private boolean parseTableMarkup(Document doc, DirVisitor visitor) {
     boolean result = false;
     for (Element row : doc.select("table tr:not(th)")) {
       final Element name = row.selectFirst("td a");
@@ -108,7 +126,7 @@ final class RemoteCatalog extends Catalog {
         ...
       </pre>
   */
-  private boolean parsePreMarkup(Document doc, DirVisitor visitor) throws IOException {
+  private boolean parsePreMarkup(Document doc, DirVisitor visitor) {
     final String DIR_SIZE = "-";
     boolean result = false;
     for (Element name : doc.select("pre > img + a")) {
@@ -135,13 +153,13 @@ final class RemoteCatalog extends Catalog {
     //text may be truncated/ellipsized/etc
     final int MIN_MATCHED_SYMBOLS = 16;
     return href.regionMatches(0, text, 0, Math.min(href.length(), MIN_MATCHED_SYMBOLS))
-            ? href
-            : null;
+        ? href
+        : null;
   }
 
   private static String trimTrailingSlash(String val) {
     return val.endsWith("/")
-            ? val.substring(0, val.length() - 1)
-            : val;
+        ? val.substring(0, val.length() - 1)
+        : val;
   }
 }
