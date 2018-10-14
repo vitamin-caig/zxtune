d192befd42fb206ecfb2a771dbaa6d0960f43e4d
vitamin-caig <vitamin.caig@gmail.com>
2018-10-15 01:53:25 +0300

    zxtune-android: fixup DialogFragment usage for RingtoneFragment and AboutFragment.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/ui/RingtoneFragment.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/ui/RingtoneFragment.java
@@ -11,22 +11,23 @@
 package app.zxtune.ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import java.util.concurrent.TimeUnit;
-
 import app.zxtune.Permission;
 import app.zxtune.R;
 import app.zxtune.RingtoneService;
 import app.zxtune.TimeStamp;
 import app.zxtune.playback.Item;
+
+import java.util.concurrent.TimeUnit;
 
 public class RingtoneFragment extends DialogFragment {
   
@@ -43,18 +44,23 @@ public class RingtoneFragment extends DialogFragment {
     result.setArguments(args);
     return result;
   }
-  
+
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    getDialog().setTitle(R.string.ringtone_create_title);
-    
-    final View result = inflater.inflate(R.layout.ringtone, container, false);
-    
-    final ViewGroup buttons = (ViewGroup) result.findViewById(R.id.ringtone_durations);
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    final Context ctx = getActivity();
+
+    final View view = ((Activity) ctx).getLayoutInflater().inflate(R.layout.ringtone, null, false);
+
+    final ViewGroup buttons = (ViewGroup) view.findViewById(R.id.ringtone_durations);
     fillDurations(buttons);
+
+    final AlertDialog result = new AlertDialog.Builder(ctx)
+        .setTitle(R.string.ringtone_create_title)
+        .setView(view)
+        .create();
     return result;
   }
-  
+
   private void fillDurations(ViewGroup container) {
 
     final Uri uri = getArguments().getParcelable("url");
