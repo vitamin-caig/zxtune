7a682644b30e39aaea630a658ed7f3fb65a87499
vitamin-caig <vitamin.caig@gmail.com>
2019-03-31 17:13:52 +0300

    zxtune-android: report ANRs to crashlytics.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/MainApplication.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/MainApplication.java
@@ -1,20 +1,18 @@
 /**
- * 
  * @file
- *
  * @brief Main application instance
- *
  * @author vitamin.caig@gmail.com
- * 
  */
 
 package app.zxtune;
 
 import android.app.Application;
 import app.zxtune.device.ui.Notifications;
+import com.github.anrwatchdog.ANRError;
+import com.github.anrwatchdog.ANRWatchDog;
 
 public class MainApplication extends Application {
-  
+
   private static Application instance;
 
   @Override
@@ -25,8 +23,21 @@ public class MainApplication extends Application {
 
     Analytics.initialize(this);
     Notifications.setup(this);
+    setupANRWatchdog();
   }
-  
+
+  private void setupANRWatchdog() {
+    // Report only main thread due to way too big report - https://github.com/SalomonBrys/ANR-WatchDog/issues/29
+    new ANRWatchDog(2000)
+        .setReportMainThreadOnly()
+        .setANRListener(new ANRWatchDog.ANRListener() {
+      @Override
+      public void onAppNotResponding(ANRError error) {
+        Log.w("ANR", error, "Hangup");
+      }
+    }).start();
+  }
+
   public static Application getInstance() {
     return instance;
   }
