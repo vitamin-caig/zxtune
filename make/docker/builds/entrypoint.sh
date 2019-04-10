7c75ac9901c7419f3eee5f0ea30baa0a02b0787d
vitamin-caig <vitamin.caig@gmail.com>
2019-04-10 03:05:52 +0300

    Initial docker scripts for linux builders.

---
+++ make/docker/builds/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+while [ "${1:0:2}" = "--" ]
+do
+  declare ${1:2}
+  shift
+done
+
+if [ -n "${append_from_stdin:-}" ]
+then
+  echo "Append ${append_from_stdin} from stdin:"
+  cat - >> "${append_from_stdin}"
+fi
+
+echo "Pull ${branch:-master}"
+git pull origin ${branch:-master}
+make -j$(grep -c processor /proc/cpuinfo) $*
