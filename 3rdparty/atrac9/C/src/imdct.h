8a29a1561c9ab0e6a7a0d20f9d60497754eca3fa
vitamin-caig <vitamin.caig@gmail.com>
2020-01-23 22:42:59 +0300

    Get rid of LibAtrac9 library. F.

--- 3rdparty/atrac9/C/src/imdct.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "structures.h"
-
-void RunImdct(Mdct* mdct, double* input, double* output);
--- 3rdparty/atrac9/C/src/unpack.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "bit_reader.h"
-#include "error_codes.h"
-#include "structures.h"
-
-At9Status UnpackFrame(Frame* frame, BitReaderCxt* br);
