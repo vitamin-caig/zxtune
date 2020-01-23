665ff66659cf77c735f5029502a47f9ebe75bd11
vitamin-caig <vitamin.caig@gmail.com>
2020-01-23 14:27:51 +0300

    ffmpeg: fix build for mingw-w64 4.8.1.

--- 3rdparty/ffmpeg/stubs/log_stub.c
+++ 3rdparty/ffmpeg/stubs/log_stub.c
@@ -7,6 +7,10 @@ const char *av_default_item_name(void *ptr) {
 void av_log(void* avcl, int level, const char *fmt, ...) {
 }
 
+int av_log_get_level(void) {
+    return AV_LOG_QUIET;
+}
+
 void avpriv_request_sample(void *avc, const char *msg, ...) {
 }
 
