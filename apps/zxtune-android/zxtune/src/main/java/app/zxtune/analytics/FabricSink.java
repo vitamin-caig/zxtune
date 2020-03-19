c03d1b8c092f81059c29a3a757f3e5cfef697b8e
vitamin-caig <vitamin.caig@gmail.com>
2020-03-19 23:49:11 +0300

    zxtune-android: add system-specific tags for crashlytics.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/analytics/FabricSink.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/analytics/FabricSink.java
@@ -1,7 +1,12 @@
 package app.zxtune.analytics;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.net.Uri;
+import android.os.Build;
+import android.os.LocaleList;
+
 import app.zxtune.BuildConfig;
 import app.zxtune.Log;
 import app.zxtune.TimeStamp;
@@ -31,6 +36,33 @@ final class FabricSink implements Sink {
 
   FabricSink(Context ctx) {
     Fabric.with(ctx, new Crashlytics(), new CrashlyticsNdk(), new Answers());
+    setTags(ctx);
+  }
+
+  private void setTags(Context ctx) {
+    try {
+      Crashlytics.setString("installer", getInstaller(ctx));
+      Crashlytics.setString("arch", Build.CPU_ABI);
+      Crashlytics.setString("locale", getLocale(ctx));
+    } catch (Throwable e) {
+      logException(e);
+    }
+  }
+
+  private static String getInstaller(Context ctx) {
+    final PackageManager pm = ctx.getPackageManager();
+    final String res = pm.getInstallerPackageName(ctx.getPackageName());
+    return res != null ? res : "unknown";
+  }
+
+  private static String getLocale(Context ctx) {
+    final Configuration cfg = ctx.getResources().getConfiguration();
+    if (Build.VERSION.SDK_INT >= 24) {
+      final LocaleList locales = cfg.getLocales();
+      return locales.get(0).toString();
+    } else {
+      return cfg.locale.toString();
+    }
   }
 
   @Override
