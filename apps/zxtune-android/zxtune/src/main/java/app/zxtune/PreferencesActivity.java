d1858570c6352db7e215153691f91e122c29ae4f
vitamin-caig <vitamin.caig@gmail.com>
2021-04-30 23:08:14 +0300

    zxtune-android: migrate to androidx.preferences. Rework dialog.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/PreferencesActivity.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/PreferencesActivity.java
@@ -1,91 +1,133 @@
 /**
- *
  * @file
- *
  * @brief Preferences activity
- *
  * @author vitamin.caig@gmail.com
- *
  */
 
 package app.zxtune;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceGroup;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.XmlRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
 
-public class PreferencesActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class PreferencesActivity extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
   public static final String ACTION_PREFERENCE_CHANGED = PreferencesActivity.class.getName() + ".PREFERENCE_CHANGED";
   public static final String EXTRA_PREFERENCE_NAME = "name";
   public static final String EXTRA_PREFERENCE_VALUE = "value";
-    
+
+  private Releaseable broadcast = ReleaseableStub.instance();
+
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    //empty now
-    //addPreferencesFromResource(R.xml.preferences_ui);
-    addPreferencesFromResource(R.xml.preferences_emulation);
-    //not used now
-    //addPreferencesFromResource(R.xml.preferences_sound);
-    initPreferenceSummary(getPreferenceScreen());
-  }
-  
-  @Override
-  protected void onResume() {
-    super.onResume();
-    getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-  }
-  
-  @Override
-  protected void onPause() {
-    super.onPause();
-    getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+
+    // Need to only create the first fragment
+    FragmentManager mgr = getSupportFragmentManager();
+    if (mgr.findFragmentById(android.R.id.content) == null) {
+      mgr.beginTransaction()
+          .add(android.R.id.content, new MainFragment())
+          .disallowAddToBackStack()
+          .commit();
+    }
   }
 
   @Override
-  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-    updatePreferenceSummary(findPreference(key));
-    final Intent intent = new Intent(ACTION_PREFERENCE_CHANGED);
-    intent.putExtra(EXTRA_PREFERENCE_NAME, key);
-    final Object val = getPreferenceScreen().getSharedPreferences().getAll().get(key);
-    if (val instanceof String) {
-      intent.putExtra(EXTRA_PREFERENCE_VALUE, (String) val);
-    } else if (val instanceof Integer) {
-      intent.putExtra(EXTRA_PREFERENCE_VALUE, (Integer) val);
-    } else if (val instanceof Long) {
-      intent.putExtra(EXTRA_PREFERENCE_VALUE, (Long) val);
-    } else if (val instanceof Boolean) {
-      intent.putExtra(EXTRA_PREFERENCE_VALUE, (Boolean) val);
-    }
-    sendBroadcast(intent);
+  public void onResume() {
+    super.onResume();
+
+    broadcast = SharedPreferenceBroadcast.subscribe(this, Preferences.getDefaultSharedPreferences(this));
   }
-  
-  private static void initPreferenceSummary(PreferenceGroup group) {
-    for (int idx = group.getPreferenceCount(); idx > 0; --idx) {
-      initPreferenceSummary(group.getPreference(idx - 1));
+
+  @Override
+  public void onPause() {
+    super.onPause();
+
+    broadcast.release();
+    broadcast = ReleaseableStub.instance();
+  }
+
+  @Override
+  public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
+    if ("zxtune.sound.mixer.3".equals(pref.getKey())) {
+      getSupportFragmentManager()
+          .beginTransaction()
+          .replace(android.R.id.content, new Mixer3Fragment())
+          .addToBackStack(null)
+          .commit();
+      return true;
+    }
+    return false;
+  }
+
+  public static class MainFragment extends Fragment {
+    public MainFragment() {
+      super(R.xml.preferences);
     }
   }
-  
-  private static void initPreferenceSummary(Preference pref) {
-    if (pref instanceof PreferenceGroup) {
-      initPreferenceSummary((PreferenceGroup) pref);
-    } else {
-      updatePreferenceSummary(pref);
+
+  public static class Mixer3Fragment extends Fragment {
+    public Mixer3Fragment() {
+      super(R.xml.preferences_mixer3);
     }
   }
-  
-  private static void updatePreferenceSummary(Preference pref) {
-    if (pref instanceof ListPreference) {
-      final ListPreference list = (ListPreference) pref;
-      list.setSummary(list.getEntry());
+
+  public static class Fragment extends PreferenceFragmentCompat {
+
+    @XmlRes
+    private final int layout;
+
+    protected Fragment(@XmlRes int layout) {
+      this.layout = layout;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+      setPreferencesFromResource(layout, rootKey);
+    }
+  }
+
+  private static class SharedPreferenceBroadcast implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private final SharedPreferences preferences;
+    private final Context context;
+
+    static Releaseable subscribe(Context ctx, SharedPreferences prefs) {
+      SharedPreferenceBroadcast self = new SharedPreferenceBroadcast(prefs, ctx);
+      prefs.registerOnSharedPreferenceChangeListener(self);
+      return () -> {
+        prefs.unregisterOnSharedPreferenceChangeListener(self);
+      };
+    }
+
+    private SharedPreferenceBroadcast(SharedPreferences prefs, Context ctx) {
+      preferences = prefs;
+      context = ctx;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+      final Intent intent = new Intent(ACTION_PREFERENCE_CHANGED);
+      intent.putExtra(EXTRA_PREFERENCE_NAME, key);
+      final Object val = preferences.getAll().get(key);
+      if (val instanceof String) {
+        intent.putExtra(EXTRA_PREFERENCE_VALUE, (String) val);
+      } else if (val instanceof Integer) {
+        intent.putExtra(EXTRA_PREFERENCE_VALUE, (Integer) val);
+      } else if (val instanceof Long) {
+        intent.putExtra(EXTRA_PREFERENCE_VALUE, (Long) val);
+      } else if (val instanceof Boolean) {
+        intent.putExtra(EXTRA_PREFERENCE_VALUE, (Boolean) val);
+      }
+      context.sendBroadcast(intent);
     }
   }
 }
