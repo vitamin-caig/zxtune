f03fa4a695d5afc542e6b5973ca646c59eec121d
vitamin-caig <vitamin.caig@gmail.com>
2021-05-06 23:25:30 +0300

    zxtune-android: fix crash in analytics.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/analytics/internal/ProviderClient.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/analytics/internal/ProviderClient.java
@@ -3,9 +3,12 @@ package app.zxtune.analytics.internal;
 import android.content.ContentResolver;
 import android.content.Context;
 
+import app.zxtune.Log;
 import app.zxtune.utils.AsyncWorker;
 
 final class ProviderClient implements UrlsSink {
+
+  private static final String TAG = ProviderClient.class.getName();
 
   private final AsyncWorker worker = new AsyncWorker("IASender");
   private final ContentResolver resolver;
@@ -16,6 +19,14 @@ final class ProviderClient implements UrlsSink {
 
   @Override
   public void push(String msg) {
-    worker.execute(() -> resolver.call(Provider.URI, Provider.METHOD_PUSH, msg, null));
+    worker.execute(() -> doPush(msg));
+  }
+
+  private void doPush(String msg) {
+    try {
+      resolver.call(Provider.URI, Provider.METHOD_PUSH, msg, null);
+    } catch (Exception e) {
+      Log.w(TAG, e, "Failed to push url");
+    }
   }
 }
