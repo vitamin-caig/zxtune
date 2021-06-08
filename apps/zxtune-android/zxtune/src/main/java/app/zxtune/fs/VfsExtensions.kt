c2699fd9e2877c1936fb8b04c0482f1f17189477
vitamin-caig <vitamin.caig@gmail.com>
2021-06-09 00:07:56 +0300

    zxtune-android: migrate fs.VfsExtensions to kotlin.

---
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/VfsExtensions.kt
@@ -0,0 +1,50 @@
+/**
+ * @file
+ * @brief
+ * @author vitamin.caig@gmail.com
+ */
+package app.zxtune.fs
+
+import java.io.IOException
+
+object VfsExtensions {
+    // Remote autoplay URI, string
+    const val SHARE_URL = "SHARE_URL"
+    const val SEARCH_ENGINE = "SEARCH_ENGINE"
+
+    // Comparator<VfsObject>
+    const val COMPARATOR = "COMPARATOR"
+
+    // java.util.Iterator<VfsFile>
+    const val FEED = "FEED"
+
+    // String
+    const val CACHE_PATH = "CACHE_PATH"
+
+    // File
+    const val FILE = "FILE"
+
+    // Uri[]
+    const val DOWNLOAD_URIS = "DOWNLOAD_URIS"
+
+    // Separate interface for fast searching
+    interface SearchEngine {
+        fun interface Visitor {
+            fun onFile(file: VfsFile)
+        }
+
+        @Throws(IOException::class)
+        fun find(query: String, visitor: Visitor)
+    }
+}
+
+val VfsDir.searchEngine
+    get() = getExtension(VfsExtensions.SEARCH_ENGINE) as? VfsExtensions.SearchEngine
+
+val VfsDir.comparator
+    @Suppress("UNCHECKED_CAST")
+    get() = getExtension(VfsExtensions.COMPARATOR) as? Comparator<VfsObject>
+
+val VfsDir.feed
+    @Suppress("UNCHECKED_CAST")
+    get() = getExtension(VfsExtensions.FEED) as? Iterator<VfsFile>
