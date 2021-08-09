36e6b3c42810706e7ded361f9f45f855daf113d6
vitamin-caig <vitamin.caig@gmail.com>
2021-08-09 23:42:33 +0300

    zxtune-android: migrate fs.amp.Identifier to kotlin.

---
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/amp/Identifier.kt
@@ -0,0 +1,119 @@
+/**
+ * @file
+ * @brief
+ * @author vitamin.caig@gmail.com
+ */
+package app.zxtune.fs.amp
+
+import android.net.Uri
+
+/**
+ * Paths:
+ *
+ *
+ * 1) amp:/
+ * 2) amp:/Handle
+ * 3) amp:/Handle/${author_letter}
+ * 4) amp:/Handle/${author_letter}/${author_name}?author=${author_id}
+ * 5) amp:/Country
+ * 6) amp:/Country/${country_name}?country=${country_id}
+ * 7) amp:/Country/${country_name}/${author_name}?author=${author_id}&country=${country_id}
+ * 8) amp:/Group
+ * 9) amp:/Group/${group_name}?group=${group_id}
+ * 10) amp:/Group/${group_name}/${author_name}?author=${author_id}&group=${group_id}
+ */
+object Identifier {
+    private const val SCHEME = "amp"
+    private const val POS_CATEGORY = 0
+    private const val POS_HANDLE_LETTER = 1
+    private const val POS_COUNTRY_NAME = 1
+    private const val POS_GROUP_NAME = 1
+    private const val POS_AUTHOR_NAME = 2
+    private const val POS_TRACK_FILENAME = 3
+    private const val PARAM_COUNTRY = "country"
+    private const val PARAM_GROUP = "group"
+    private const val PARAM_AUTHOR = "author"
+    private const val PARAM_TRACK = "track"
+    const val CATEGORY_HANDLE = "Handle"
+    const val CATEGORY_COUNTRY = "Country"
+    const val CATEGORY_GROUP = "Group"
+
+    // Root
+    @JvmStatic
+    fun forRoot(): Uri.Builder = Uri.Builder().scheme(SCHEME)
+
+    @JvmStatic
+    fun isFromRoot(uri: Uri) = SCHEME == uri.scheme
+
+    // Categories
+    @JvmStatic
+    fun forCategory(category: String): Uri.Builder = forRoot().appendPath(category)
+
+    @JvmStatic
+    fun findCategory(path: List<String>) = path.getOrNull(POS_CATEGORY)
+
+    // Handles
+    @JvmStatic
+    fun forHandleLetter(letter: String): Uri.Builder =
+        forCategory(CATEGORY_HANDLE).appendPath(letter)
+
+    @JvmStatic
+    fun isHandleLetter(letter: String) =
+        letter.length == 1 && Character.isLetter(letter[0]) && Character.isUpperCase(letter[0])
+
+    @JvmStatic
+    fun findHandleLetter(uri: Uri, path: List<String>) = path.getOrNull(POS_HANDLE_LETTER)
+        ?.takeIf { Catalog.NON_LETTER_FILTER == it || isHandleLetter(it) }
+
+    // Countries
+    @JvmStatic
+    fun forCountry(country: Country): Uri.Builder = forCategory(CATEGORY_COUNTRY)
+        .appendPath(country.name)
+        .appendQueryParameter(PARAM_COUNTRY, country.id.toString())
+
+    @JvmStatic
+    fun findCountry(uri: Uri, path: List<String>) = path.getOrNull(POS_COUNTRY_NAME)?.let { name ->
+        uri.getQueryParameter(PARAM_COUNTRY)?.toIntOrNull()?.let { id ->
+            Country(id, name)
+        }
+    }
+
+    // Groups
+    @JvmStatic
+    fun forGroup(group: Group): Uri.Builder = forCategory(CATEGORY_GROUP)
+        .appendPath(group.name)
+        .appendQueryParameter(PARAM_GROUP, group.id.toString())
+
+    @JvmStatic
+    fun findGroup(uri: Uri, path: List<String>) = path.getOrNull(POS_GROUP_NAME)?.let { name ->
+        uri.getQueryParameter(PARAM_GROUP)?.toIntOrNull()?.let { id ->
+            Group(id, name)
+        }
+    }
+
+    // Authors
+    @JvmStatic
+    fun forAuthor(parent: Uri.Builder, author: Author): Uri.Builder =
+        parent.appendPath(author.handle)
+            .appendQueryParameter(PARAM_AUTHOR, author.id.toString())
+
+    @JvmStatic
+    fun findAuthor(uri: Uri, path: List<String>) = path.getOrNull(POS_AUTHOR_NAME)?.let { name ->
+        uri.getQueryParameter(PARAM_AUTHOR)?.toIntOrNull()?.let { id ->
+            Author(id, name, ""/*fake*/)
+        }
+    }
+
+    // Tracks
+    @JvmStatic
+    fun forTrack(parent: Uri.Builder, track: Track): Uri.Builder = parent.appendPath(track.filename)
+        .appendQueryParameter(PARAM_TRACK, track.id.toString())
+
+    @JvmStatic
+    fun findTrack(uri: Uri, path: List<String>) =
+        path.getOrNull(POS_TRACK_FILENAME)?.let { filename ->
+            uri.getQueryParameter(PARAM_TRACK)?.toIntOrNull()?.let { id ->
+                Track(id, filename, 0/*fake*/)
+            }
+        }
+}
