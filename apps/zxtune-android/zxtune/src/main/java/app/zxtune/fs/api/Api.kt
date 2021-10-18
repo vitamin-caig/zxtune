6dda66435fa477eb1a14a514fef3281209cca6ef
vitamin-caig <vitamin.caig@gmail.com>
2021-10-18 10:22:20 +0300

    zxtune-android: migrate fs.api.Api to kotlin.

---
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/api/Api.kt
@@ -0,0 +1,59 @@
+package app.zxtune.fs.api
+
+import android.content.Context
+import android.util.Base64
+import androidx.annotation.VisibleForTesting
+import app.zxtune.BuildConfig
+import app.zxtune.auth.Auth
+import app.zxtune.net.Http
+import java.io.IOException
+import java.net.HttpURLConnection
+
+private const val ENDPOINT = BuildConfig.API_ROOT + "/"
+private const val REPLY = ""
+
+object Api {
+    @VisibleForTesting
+    var authorization: String? = null
+
+    @JvmStatic
+    fun initialize(ctx: Context) = setAuthorization(Auth.getUserIdentifier(ctx), "")
+
+    @VisibleForTesting
+    fun setAuthorization(name: String, password: String) {
+        val credentials = "$name:$password"
+        authorization = "Basic ${Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)}"
+    }
+
+    @Throws(IOException::class)
+    @JvmStatic
+    fun postEvent(url: String) {
+        val reply = doRequest("${ENDPOINT}events/$url", "POST")
+        if (REPLY != reply) {
+            throw IOException("Wrong reply: $reply")
+        }
+    }
+
+    private fun doRequest(fullUrl: String, method: String) = with(Http.createConnection(fullUrl)) {
+        authorization?.let {
+            setRequestProperty("Authorization", it)
+        }
+        requestMethod = method
+        try {
+            connect()
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                throw IOException(responseMessage)
+            }
+            inputStream.use { input ->
+                val realUrl = url
+                // Allow redirects to the same origin
+                if (!realUrl.toString().startsWith(ENDPOINT)) {
+                    throw IOException("Unexpected redirect: $fullUrl -> $realUrl")
+                }
+                input.bufferedReader().use { it.readText() }
+            }
+        } finally {
+            disconnect()
+        }
+    }
+}
--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/api/package-info.java
+++
@@ -1,5 +0,0 @@
-@NonNullByDefault
-package app.zxtune.fs.api;
-
-import app.zxtune.annotations.NonNullByDefault;
-
