1a139dea150ddf5df06cfd850797a90cc410401b
vitamin-caig <vitamin.caig@gmail.com>
2021-10-28 00:36:33 +0300

    Remove multitrack adapters in formats_archived_multitrack.

---
+++ src/formats/archived/multitrack/filename.cpp
@@ -0,0 +1,35 @@
+/**
+ *
+ * @file
+ *
+ * @brief  Multitrack archives support implementation
+ *
+ * @author vitamin.caig@gmail.com
+ *
+ **/
+
+// local includes
+#include "formats/archived/multitrack/filename.h"
+// library includes
+#include <strings/prefixed_index.h>
+
+namespace Formats::Archived::MultitrackArchives
+{
+  const auto FILENAME_PREFIX = "#"_sv;
+
+  String CreateFilename(std::size_t index)
+  {
+    return Strings::PrefixedIndex(FILENAME_PREFIX, index).ToString();
+  }
+
+  std::optional<std::size_t> ParseFilename(StringView str)
+  {
+    const auto parsed = Strings::PrefixedIndex(FILENAME_PREFIX, str);
+    std::optional<std::size_t> result;
+    if (parsed.IsValid())
+    {
+      result = parsed.GetIndex();
+    }
+    return result;
+  }
+}  // namespace Formats::Archived::MultitrackArchives
