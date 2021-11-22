4a9f89f9259a979cdd89039c46888692e7bdbb9b
vitamin-caig <vitamin.caig@gmail.com>
2021-11-23 00:33:53 +0300

    ogg: initial import v1.3.5 from https://gitlab.xiph.org/xiph/ogg.git.

--- 3rdparty/ogg/config_types.h
+++ 3rdparty/ogg/config_types.h
@@ -1,9 +1,13 @@
-#include <bits/wordsize.h>
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
 
-#if __WORDSIZE == 32
-#include "config_types-32.h"
-#elif __WORDSIZE == 64
-#include "config_types-64.h"
-#else
-#error "Unknown word size"
+#include <stdint.h>
+
+typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef uint32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+typedef uint64_t ogg_uint64_t;
+
 #endif
