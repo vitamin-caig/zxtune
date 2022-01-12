58872f06ee951e091c7d1329b5604ee8f141e882
vitamin-caig <vitamin.caig@gmail.com>
2022-01-13 00:08:36 +0300

    zxtune-android: add fs.provider.ListingCursorBuilderTest.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/provider/ListingCursorBuilder.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/provider/ListingCursorBuilder.java
@@ -2,31 +2,26 @@ package app.zxtune.fs.provider;
 
 import android.database.Cursor;
 import android.database.MatrixCursor;
-import android.net.Uri;
 
 import androidx.annotation.Nullable;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.concurrent.CancellationException;
 
 import app.zxtune.fs.DefaultComparator;
-import app.zxtune.fs.Vfs;
 import app.zxtune.fs.VfsDir;
-import app.zxtune.fs.VfsExtensions;
 import app.zxtune.fs.VfsFile;
 import app.zxtune.fs.VfsObject;
-import app.zxtune.fs.VfsUtils;
 
 class ListingCursorBuilder extends VfsDir.Visitor {
 
   private final SchemaSource schema;
   private final ArrayList<VfsDir> dirs = new ArrayList<>();
   private final ArrayList<VfsFile> files = new ArrayList<>();
-  private int total = -1;
-  private int done = -1;
+  private int total = 0;
+  private int done = 0;
 
   ListingCursorBuilder(SchemaSource schema) {
     this.schema = schema;
@@ -81,45 +76,12 @@ class ListingCursorBuilder extends VfsDir.Visitor {
     return cursor;
   }
 
-  private static Object[] makeRow(VfsDir d) {
-    return Schema.Listing.makeDirectory(d.getUri(), d.getName(),
-        d.getDescription(), VfsUtils.getObjectIcon(d), hasFeed(d));
-  }
-
-  private static Object[] makeRow(Uri uri, VfsFile f, @Nullable Integer tracks) {
-    return Schema.Listing.makeFile(uri, f.getName(), f.getDescription(), f.getSize(), tracks,
-        isCached(f));
-  }
-
-  private static boolean hasFeed(VfsObject obj) {
-    return null != obj.getExtension(VfsExtensions.FEED);
-  }
-
-  @Nullable
-  private static Boolean isCached(VfsFile obj) {
-    final File f = Vfs.getCache(obj);
-    return f != null ? f.isFile() : null;
-  }
-
   final Cursor getStatus() {
     final MatrixCursor cursor = new MatrixCursor(Schema.Status.COLUMNS, 1);
     if (total != 0) {
       cursor.addRow(new Schema.Status.Progress(done, total).serialize());
     } else {
       cursor.addRow(Schema.Status.Progress.Companion.createIntermediate().serialize());
-    }
-    return cursor;
-  }
-
-  //TODO: check out for separate schema
-  static Cursor createSingleEntry(VfsObject obj) {
-    final MatrixCursor cursor = new MatrixCursor(Schema.Listing.COLUMNS, 1);
-    if (obj instanceof VfsDir) {
-      cursor.addRow(makeRow((VfsDir) obj));
-    } else if (obj instanceof VfsFile) {
-      final Uri uri = obj.getUri();
-      // don't care about real tracks count here
-      cursor.addRow(makeRow(uri, (VfsFile) obj, null));
     }
     return cursor;
   }
