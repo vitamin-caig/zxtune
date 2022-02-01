25236d2bf02921787163581e7cdd316518eaf856
vitamin-caig <vitamin.caig@gmail.com>
2022-02-02 01:49:45 +0300

    zxtune-android: cleanup vgmrips.Pack.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/vgmrips/Pack.kt
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/vgmrips/Pack.kt
@@ -4,10 +4,7 @@ package app.zxtune.fs.vgmrips
 data class Pack(
     val id: String,
     val title: String,
-    var songs: Int,
-    var score: Int,
-    var ratings: Int
-) {
-    // TODO: use only primary ctor with default values
-    constructor(id: String, title: String) : this(id, title, 0, 0, 0)
-}
+    var songs: Int = 0,
+    var score: Int = 0,
+    var ratings: Int = 0
+)
