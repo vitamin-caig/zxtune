a52f2ecd7ab89e7757ba1155baa0255ddbdd00ad
vitamin-caig <vitamin.caig@gmail.com>
2022-02-02 01:49:27 +0300

    zxtune-android: cleanup zxart.Track.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/zxart/Track.kt
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/zxart/Track.kt
@@ -8,13 +8,10 @@ package app.zxtune.fs.zxart
 data class Track(
     val id: Int,
     val filename: String,
-    val title: String,
-    val votes: String,
-    val duration: String,
-    val year: Int,
-    val compo: String,
-    val partyplace: Int
-) {
-    // TODO: use default parameters
-    constructor(id: Int, filename: String) : this(id, filename, "", "", "", 0, "", 0)
-}
+    val title: String = "",
+    val votes: String = "",
+    val duration: String = "",
+    val year: Int = 0,
+    val compo: String = "",
+    val partyplace: Int = 0
+)
