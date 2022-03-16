3db0a79ab6873ad3ecdcac55a76e70aae8f3e79e
vitamin-caig <vitamin.caig@gmail.com>
2022-03-17 00:37:57 +0300

    zxtune-android: add browser notification about new playlists creation problems.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/provider/NotificationsSource.kt
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/provider/NotificationsSource.kt
@@ -38,11 +38,19 @@ internal class NotificationsSource @VisibleForTesting constructor(
     }
 
     @VisibleForTesting
-    fun getNotification(uri: Uri) = getNetworkNotification(uri) ?: getStorageNotification(uri)
+    fun getNotification(uri: Uri): Schema.Notifications.Object? {
+        resolverNotificationUri = null
+        return when (uri.scheme) {
+            null -> null // root
+            "file" -> getStorageNotification(uri)
+            "playlists" -> getPlaylistNotification(uri)
+            else -> getNetworkNotification(uri)
+        }
+    }
 
     private fun getNetworkNotification(uri: Uri): Schema.Notifications.Object? {
-        resolverNotificationUri = if (isNetwork(uri)) Query.notificationUriFor(uri) else null
-        return if (resolverNotificationUri != null && false == networkState.value) {
+        resolverNotificationUri = Query.notificationUriFor(uri)
+        return if (false == networkState.value) {
             Schema.Notifications.Object(
                 ctx.getString(R.string.network_inaccessible),
                 Intent(Settings.ACTION_WIRELESS_SETTINGS)
@@ -53,8 +61,7 @@ internal class NotificationsSource @VisibleForTesting constructor(
     }
 
     private fun getStorageNotification(uri: Uri) =
-        if ("file" == uri.scheme &&
-            Build.VERSION.SDK_INT >= 30 &&
+        if (Build.VERSION.SDK_INT >= 30 &&
             true == uri.path?.takeIf { it.isNotEmpty() }?.let {
                 !Environment.isExternalStorageManager(File(it))
             }
@@ -67,15 +74,20 @@ internal class NotificationsSource @VisibleForTesting constructor(
             null
         }
 
+    private fun getPlaylistNotification(uri: Uri) =
+        if (Build.VERSION.SDK_INT >= 30 && uri.path.isNullOrEmpty() && !Environment.isExternalStorageManager()) {
+            Schema.Notifications.Object(
+                ctx.getString(R.string.limited_stored_playlists_access),
+                Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+            )
+        } else {
+            null
+        }
+
     companion object {
         private fun getNetworkState(ctx: Context): LiveData<Boolean> {
             NetworkManager.initialize(ctx.applicationContext)
             return NetworkManager.networkAvailable
-        }
-
-        private fun isNetwork(uri: Uri) = when (uri.scheme) {
-            null, "file", "playlists" -> false
-            else -> true
         }
     }
 }
