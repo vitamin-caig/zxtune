319b8ace697bdb9093c3c7f743116bf09456b09f
vitamin-caig <vitamin.caig@gmail.com>
2023-02-02 21:31:18 +0300

    ffmpeg: disable asserts with debug messages.

--- 3rdparty/ffmpeg/libavutil/avassert.h
+++ 3rdparty/ffmpeg/libavutil/avassert.h
@@ -34,6 +34,7 @@
 /**
  * assert() equivalent, that is always enabled.
  */
+#if defined(ASSERT_LEVEL)
 #define av_assert0(cond) do {                                           \
     if (!(cond)) {                                                      \
         av_log(NULL, AV_LOG_PANIC, "Assertion %s failed at %s:%d\n",    \
@@ -41,7 +42,9 @@
         abort();                                                        \
     }                                                                   \
 } while (0)
-
+#else
+#define av_assert0(cond) ((void)0)
+#endif
 
 /**
  * assert() equivalent, that does not lie in speed critical code.
