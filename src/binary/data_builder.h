8bb74beb25db56746be15b96aa2922f126f6b3cb
vitamin-caig <vitamin.caig@gmail.com>
2023-04-05 19:02:15 +0300

    Improve Binary::DataBuilder.

--- src/binary/data_builder.h
+++ src/binary/data_builder.h
@@ -23,14 +23,11 @@ namespace Binary
   class DataBuilder
   {
   public:
-    DataBuilder()
-      : Content(new Dump())
-    {}
+    DataBuilder() {}
 
     explicit DataBuilder(std::size_t reserve)
-      : Content(new Dump())
     {
-      Content->reserve(reserve);
+      Content.reserve(reserve);
     }
 
     template<class T, typename std::enable_if<!std::is_pointer<T>::value, int>::type = 0>
@@ -46,59 +43,69 @@ namespace Binary
       std::memcpy(Allocate(sizeof(T)), &val, sizeof(val));
     }
 
-    void Add(View data)
+    void AddByte(uint8_t val)
     {
-      std::memcpy(Allocate(data.Size()), data.Start(), data.Size());
+      Content.push_back(val);
+    }
+
+    void* Add(View data)
+    {
+      return std::memcpy(Allocate(data.Size()), data.Start(), data.Size());
     }
 
     void* Allocate(std::size_t size)
     {
-      const std::size_t curSize = Content->size();
-      Content->resize(curSize + size);
+      const std::size_t curSize = Content.size();
+      Content.resize(curSize + size);
       return Get(curSize);
     }
 
-    void AddCString(const StringView str)
+    char* AddCString(const StringView str)
     {
       const std::size_t size = str.size();
       char* const dst = static_cast<char*>(Allocate(size + 1));
       std::copy(str.begin(), str.end(), dst);
       dst[size] = 0;
+      return dst;
     }
 
     std::size_t Size() const
     {
-      return Content->size();
+      return Content.size();
     }
 
-    void* Get(std::size_t offset) const
+    void* Get(std::size_t offset)
     {
-      return Content->data() + offset;
+      return Content.data() + offset;
     }
 
     template<class T>
-    T& Get(std::size_t offset) const
+    T& Get(std::size_t offset)
     {
       return *safe_ptr_cast<T*>(Get(offset));
     }
 
+    View GetView() const
+    {
+      return View(Content);
+    }
+
     void Resize(std::size_t size)
     {
-      Content->resize(size);
+      Content.resize(size);
     }
 
     void CaptureResult(Dump& res)
     {
-      Content->swap(res);
-      Content.reset();
+      res = std::move(Content);
     }
 
     Container::Ptr CaptureResult()
     {
-      return CreateContainer(std::move(Content));
+      return CreateContainer(std::make_unique<Dump>(std::move(Content)));
     }
 
   private:
-    std::unique_ptr<Dump> Content;
+    Dump Content;
   };
 }  // namespace Binary
