059871466c9cc0ef0c9ebfb4f10b95d12561f903
vitamin-caig <vitamin.caig@gmail.com>
2023-05-25 01:19:04 +0300

    zxtune-android: add ResultActivity.createStartServiceIntent trampoline.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/ResultActivity.kt
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/ResultActivity.kt
@@ -1,5 +1,6 @@
 package app.zxtune
 
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -9,7 +10,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts.OpenDocumentTree
 import app.zxtune.device.Permission
 import app.zxtune.device.PersistentStorage
-import app.zxtune.utils.ifNotNulls
 
 class ResultActivity : ComponentActivity() {
 
@@ -17,18 +17,32 @@ class ResultActivity : ComponentActivity() {
         private const val ACTION_REQUEST_STORAGE_PERMISSION = "request_storage_permission"
         private const val ACTION_REQUEST_PERSISTENT_STORAGE_LOCATION =
             "request_persistent_storage_location"
+        private const val ACTION_START_SERVICE = "start_service"
+        private const val EXTRA_ORIGINAL_COMPONENT = "extra_original_component"
+        private const val EXTRA_ORIGINAL_ACTION = "extra_original_action"
 
         fun createStoragePermissionRequestIntent(ctx: Context, treeUri: Uri) = Intent(
-            ACTION_REQUEST_STORAGE_PERMISSION,
-            treeUri,
-            ctx,
-            ResultActivity::class.java
+            ACTION_REQUEST_STORAGE_PERMISSION, treeUri, ctx, ResultActivity::class.java
         )
 
         fun createPersistentStorageLocationRequestIntent(ctx: Context, treeUri: Uri) = Intent(
-            ACTION_REQUEST_PERSISTENT_STORAGE_LOCATION, treeUri, ctx,
-            ResultActivity::class.java
+            ACTION_REQUEST_PERSISTENT_STORAGE_LOCATION, treeUri, ctx, ResultActivity::class.java
         )
+
+        // Do not store parcelable intent to avoid UnsafeIntentLaunchViolation
+        fun createStartServiceIntent(
+            ctx: Context, cls: Class<*>, act: String, uri: Uri? = null
+        ) = Intent(ctx, ResultActivity::class.java).apply {
+            action = ACTION_START_SERVICE
+            data = uri
+            putExtra(EXTRA_ORIGINAL_COMPONENT, ComponentName(ctx, cls))
+            putExtra(EXTRA_ORIGINAL_ACTION, act)
+        }
+
+        private fun getNestedIntent(src: Intent) =
+            Intent(requireNotNull(src.getStringExtra(EXTRA_ORIGINAL_ACTION)), src.data).apply {
+                component = requireNotNull(src.getParcelableExtra(EXTRA_ORIGINAL_COMPONENT))
+            }
     }
 
     private val storagePermissionRequest =
@@ -48,13 +62,20 @@ class ResultActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val action = intent?.action ?: return
+        when (action) {
+            ACTION_START_SERVICE -> {
+                startService(getNestedIntent(intent))
+                finish()
+            }
 
-        ifNotNulls(intent?.action, intent?.data, this::processIntent)
-    }
+            ACTION_REQUEST_STORAGE_PERMISSION -> storagePermissionRequest.launch(
+                requireNotNull(intent.data)
+            )
 
-    private fun processIntent(action: String, data: Uri) = when (action) {
-        ACTION_REQUEST_STORAGE_PERMISSION -> storagePermissionRequest.launch(data)
-        ACTION_REQUEST_PERSISTENT_STORAGE_LOCATION -> persistentStorageLocationRequest.launch(data)
-        else -> Unit
+            ACTION_REQUEST_PERSISTENT_STORAGE_LOCATION -> persistentStorageLocationRequest.launch(
+                requireNotNull(intent.data)
+            )
+        }
     }
 }
