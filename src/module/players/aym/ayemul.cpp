ce1925c00a354b1225cec7921974b99357315398
vitamin-caig <vitamin.caig@gmail.com>
2023-08-21 20:16:41 +0300

    Extract AYDataChannel.

--- src/module/players/aym/ayemul.cpp
+++ src/module/players/aym/ayemul.cpp
@@ -11,6 +11,7 @@
 #include "module/players/aym/ayemul.h"
 
 #include "formats/chiptune/emulation/ay.h"
+#include "module/players/aym/ay_data_channel.h"
 #include "module/players/aym/aym_base.h"
 #include "module/players/aym/aym_properties_helper.h"
 #include "module/players/duration.h"
@@ -32,95 +33,6 @@
 namespace Module::AYEMUL
 {
   const Debug::Stream Dbg("Core::AYSupp");
-
-  class AyDataChannel
-  {
-  public:
-    explicit AyDataChannel(Devices::AYM::Chip::Ptr chip)
-      : Chip(std::move(chip))
-    {}
-
-    void Reset()
-    {
-      Chip->Reset();
-      Register = 0;
-      Chunks.clear();
-      State = Devices::AYM::DataChunk();
-      Blocked = false;
-    }
-
-    void SetBlocked(bool block)
-    {
-      Blocked = block;
-    }
-
-    bool SelectRegister(uint_t reg)
-    {
-      Register = reg;
-      return IsRegisterSelected();
-    }
-
-    bool SetValue(const Devices::AYM::Stamp& timeStamp, uint8_t val)
-    {
-      if (IsRegisterSelected())
-      {
-        const auto idx = static_cast<Devices::AYM::Registers::Index>(Register);
-        if (Devices::AYM::DataChunk* chunk = GetChunk(timeStamp))
-        {
-          chunk->Data[idx] = val;
-        }
-        State.Data[idx] = val;
-        return true;
-      }
-      return false;
-    }
-
-    uint8_t GetValue() const
-    {
-      if (IsRegisterSelected())
-      {
-        return State.Data[static_cast<Devices::AYM::Registers::Index>(Register)];
-      }
-      else
-      {
-        return 0xff;
-      }
-    }
-
-    Sound::Chunk RenderFrame(const Devices::AYM::Stamp& till)
-    {
-      AllocateChunk(till);
-      Chip->RenderData(Chunks);
-      Chunks.clear();
-      return Chip->RenderTill(till);
-    }
-
-  private:
-    bool IsRegisterSelected() const
-    {
-      return Register < Devices::AYM::Registers::TOTAL;
-    }
-
-    Devices::AYM::DataChunk* GetChunk(const Devices::AYM::Stamp& timeStamp)
-    {
-      return Blocked ? nullptr : &AllocateChunk(timeStamp);
-    }
-
-    Devices::AYM::DataChunk& AllocateChunk(const Devices::AYM::Stamp& timeStamp)
-    {
-      Chunks.resize(Chunks.size() + 1);
-      Devices::AYM::DataChunk& res = Chunks.back();
-      res.TimeStamp = timeStamp;
-      return res;
-    }
-
-  private:
-    const Devices::AYM::Chip::Ptr Chip;
-    uint_t Register = 0;
-    std::vector<Devices::AYM::DataChunk> Chunks;
-    Devices::AYM::DataChunk State;
-    bool Blocked = false;
-  };
 
   class BeeperDataChannel
   {
