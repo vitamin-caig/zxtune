37d7cabf5f5b2977d891c82bffde52bea5e2ce2c
vitamin-caig <vitamin.caig@gmail.com>
2023-10-30 17:31:26 +0300

    zxtune-android: fix PagerTabStrip behaviour for some models.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/ui/ViewPagerAdapter.java
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/ui/ViewPagerAdapter.java
@@ -1,11 +1,7 @@
 /**
- * 
  * @file
- *
  * @brief PagerAdapter implementation over layout-defined ViewPager content
- *
  * @author vitamin.caig@gmail.com
- * 
  */
 
 package app.zxtune.ui;
@@ -24,10 +20,17 @@ public class ViewPagerAdapter extends PagerAdapter {
   private final String[] titles;
 
   public ViewPagerAdapter(ViewPager pager) {
-    this.delta = pager.getChildAt(0) instanceof PagerTabStrip ? 1 : 0;
+    final View tabStrip = pager.getChildAt(0);
+    if (tabStrip instanceof PagerTabStrip) {
+      // This is actual for Sony XPeria https://issuetracker.google.com/issues/37105263
+      ((ViewPager.LayoutParams) tabStrip.getLayoutParams()).isDecor = true;
+      this.delta = 1;
+    } else {
+      this.delta = 0;
+    }
     this.titles = new String[pager.getChildCount() - delta];
     for (int i = 0; i != titles.length; ++i) {
-      titles[i] = getTitle(pager.getContext(), pager.getChildAt(delta + i)); 
+      titles[i] = getTitle(pager.getContext(), pager.getChildAt(delta + i));
     }
     pager.setOffscreenPageLimit(titles.length);
   }
@@ -36,16 +39,14 @@ public class ViewPagerAdapter extends PagerAdapter {
     //only String can be stored in View's tag, so try to decode manually
     final String ID_PREFIX = "@";
     final String tag = (String) pane.getTag();
-    return tag.startsWith(ID_PREFIX)
-      ? getStringByName(ctx, tag.substring(ID_PREFIX.length()))
-      : tag;
+    return tag.startsWith(ID_PREFIX) ? getStringByName(ctx, tag.substring(ID_PREFIX.length())) : tag;
   }
-  
+
   private String getStringByName(Context ctx, String name) {
     final int id = ctx.getResources().getIdentifier(name, null, ctx.getPackageName());
     return ctx.getString(id);
   }
-  
+
   @Override
   public int getCount() {
     return titles.length;
@@ -64,9 +65,9 @@ public class ViewPagerAdapter extends PagerAdapter {
   @Override
   public void destroyItem(ViewGroup container, int position, Object object) {
   }
-  
+
   @Override
   public CharSequence getPageTitle(int position) {
-    return titles[position]; 
+    return titles[position];
   }
 }
