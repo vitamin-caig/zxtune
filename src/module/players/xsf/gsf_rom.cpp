2577c81127fd14ef8ffc224e6c82406cce506dbd
vitamin-caig <vitamin.caig@gmail.com>
2023-12-29 00:20:50 +0300

    Fix GSF setup for some tracks.
    Blender Bros.
    Crash Bandicoot 2 - N-Tranced
    Crushed Baseball
    Daredevil
    Digimon Racing
    Pirates of the Caribbean - Dead Man's Chest
    Sims 2, The
    Taiketsu! Ultra Hero
    Tantei Gakuen Q - Kyuukyoku Trick ni Idome!
    Urbz, The - Sims in the City

--- src/module/players/xsf/gsf_rom.cpp
+++ src/module/players/xsf/gsf_rom.cpp
@@ -18,6 +18,7 @@
 
 namespace Module::GSF
 {
+  // Some details at https://github.com/loveemu/gsfopt/blob/master/src/gsfopt.cpp
   class RomParser : public Formats::Chiptune::GameBoyAdvanceSoundFormat::Builder
   {
   public:
@@ -27,15 +28,68 @@ namespace Module::GSF
 
     void SetEntryPoint(uint32_t addr) override
     {
-      if (addr && !Rom.EntryPoint)
+      if (addr)
       {
-        Rom.EntryPoint = addr;
+        addr = FixEntrypoint(addr);
+        Require(addr == EWRAM_START || addr == PAKROM_START);
+        if (!Rom.EntryPoint)
+        {
+          Rom.EntryPoint = addr;
+        }
+        else
+        {
+          Require(Rom.EntryPoint == addr);
+        }
       }
     }
 
     void SetRom(uint32_t address, Binary::View content) override
     {
-      Rom.Content.Update(address & 0x1fffffff, content);
+      const auto entryArea = GetArea(Rom.EntryPoint);
+      Require(entryArea != Area::Unknown);
+      const auto realAddress = Rom.EntryPoint + (address & static_cast<uint32_t>(entryArea));
+      Rom.Content.Update(realAddress, content);
+    }
+
+  private:
+    // https://www.coranac.com/tonc/text/hardware.htm
+    static constexpr uint32_t EWRAM_START = 0x02000000;
+    static constexpr uint32_t EWRAM_SIZE = 0x40000;
+    static constexpr uint32_t PAKROM_START = 0x08000000;
+    static constexpr uint32_t PAKROM_SIZE = 0x02000000;
+
+    static uint32_t FixEntrypoint(uint32_t addr)
+    {
+      switch (addr)
+      {
+      case 0x80000000:  // Crash Bandicoot 2
+        return PAKROM_START;
+      default:
+        return addr;
+      }
+    }
+
+    enum class Area : uint32_t
+    {
+      Unknown,
+      Multiboot = EWRAM_SIZE - 1,
+      Rom = PAKROM_SIZE - 1
+    };
+
+    static Area GetArea(uint32_t addr)
+    {
+      if (addr >= EWRAM_START && addr < EWRAM_START + EWRAM_SIZE)
+      {
+        return Area::Multiboot;
+      }
+      else if (addr >= PAKROM_START && addr < PAKROM_START + PAKROM_SIZE)
+      {
+        return Area::Rom;
+      }
+      else
+      {
+        return Area::Unknown;
+      }
     }
 
   private:
