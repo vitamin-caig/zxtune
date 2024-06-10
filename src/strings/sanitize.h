d3258a79a27efbde9d6599cd124d8427d599ee07
vitamin-caig <vitamin.caig@gmail.com>
2024-06-10 17:35:26 +0300

    Add Strings::Sanitize functions.

---
+++ src/strings/sanitize.h
@@ -0,0 +1,26 @@
+/**
+ *
+ * @file
+ *
+ * @brief  String sanitize functions
+ *
+ * @author vitamin.caig@gmail.com
+ *
+ **/
+
+#pragma once
+
+// common includes
+#include <types.h>
+
+namespace Strings
+{
+  /*           | \t  | \r,\n,\r\n | ctrl | trim?
+  Sanitize     | ' ' | ' '        | none | yes
+  *Multiline   | ' ' | \n         | none | every line
+  *KeepPadding | ' ' | ' '        | ' '  | right only
+  */
+  String Sanitize(StringView str);
+  String SanitizeMultiline(StringView str);
+  String SanitizeKeepPadding(StringView str);
+}  // namespace Strings
