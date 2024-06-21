73faf65d6d12086e66586fc47fdbc3d4c4ef548e
vitamin-caig <vitamin.caig@gmail.com>
2024-06-21 17:35:12 +0300

    Extract plugin attributes to constants.

--- src/core/plugin_attrs.h
+++ src/core/plugin_attrs.h
@@ -68,88 +68,63 @@ namespace ZXTune
   {
     namespace Category
     {
-      // clang-format off
-      enum
-      {
-        MODULE    = 0x00000000,
-        CONTAINER = 0x10000000,
+      constexpr uint_t MODULE = 0x00000000;
+      constexpr uint_t CONTAINER = 0x10000000;
 
-        MASK      = 0xf0000000
-      };
-      // clang-format on
+      constexpr uint_t MASK = 0xf0000000;
     }  // namespace Category
 
     namespace Module
     {
       namespace Type
       {
-        // clang-format off
-        enum
-        {
-          TRACK      = 0x00000000,
-          STREAM     = 0x00000001,
-          MEMORYDUMP = 0x00000002,
-          MULTI      = 0x00000003,
+        constexpr uint_t TRACK = 0x00000000;
+        constexpr uint_t STREAM = 0x00000001;
+        constexpr uint_t MEMORYDUMP = 0x00000002;
+        constexpr uint_t MULTI = 0x00000003;
 
-          MASK       = 0x0000000f
-        };
-        // clang-format on
+        constexpr uint_t MASK = 0x0000000f;
       }  // namespace Type
 
       namespace Device
       {
-        // clang-format off
-        enum
-        {
-          AY38910    = 0x00000010,
-          TURBOSOUND = 0x00000020,
-          BEEPER     = 0x00000040,
-          YM2203     = 0x00000080,
-          TURBOFM    = 0x00000100,
-          DAC        = 0x00000200,
-          SAA1099    = 0x00000400,
-          MOS6581    = 0x00000800,
-          SPC700     = 0x00001000,
-          MULTI      = 0x00002000,
-          RP2A0X     = 0x00004000,
-          LR35902    = 0x00008000,
-          CO12294    = 0x00010000,
-          HUC6270    = 0x00020000,
+        constexpr uint_t AY38910 = 0x00000010;
+        constexpr uint_t TURBOSOUND = 0x00000020;
+        constexpr uint_t BEEPER = 0x00000040;
+        constexpr uint_t YM2203 = 0x00000080;
+        constexpr uint_t TURBOFM = 0x00000100;
+        constexpr uint_t DAC = 0x00000200;
+        constexpr uint_t SAA1099 = 0x00000400;
+        constexpr uint_t MOS6581 = 0x00000800;
+        constexpr uint_t SPC700 = 0x00001000;
+        constexpr uint_t MULTI = 0x00002000;
+        constexpr uint_t RP2A0X = 0x00004000;
+        constexpr uint_t LR35902 = 0x00008000;
+        constexpr uint_t CO12294 = 0x00010000;
+        constexpr uint_t HUC6270 = 0x00020000;
 
-          MASK       = 0x000ffff0
-        };
-        // clang-format on
+        constexpr uint_t MASK = 0x000ffff0;
       }  // namespace Device
 
       namespace Conversion
       {
-        // clang-format off
-        enum
-        {
-          OUT        = 0x00100000,
-          PSG        = 0x00200000,
-          YM         = 0x00400000,
-          ZX50       = 0x00800000,
-          TXT        = 0x01000000,
-          AYDUMP     = 0x02000000,
-          FYM        = 0x04000000,
+        constexpr uint_t OUT = 0x00100000;
+        constexpr uint_t PSG = 0x00200000;
+        constexpr uint_t YM = 0x00400000;
+        constexpr uint_t ZX50 = 0x00800000;
+        constexpr uint_t TXT = 0x01000000;
+        constexpr uint_t AYDUMP = 0x02000000;
+        constexpr uint_t FYM = 0x04000000;
 
-          MASK       = 0x07f00000
-        };
-        // clang-format on
+        constexpr uint_t MASK = 0x07f00000;
       }  // namespace Conversion
 
       namespace Traits
       {
-        // clang-format off
-        enum
-        {
-          //! Plugin may produce multifile tracks
-          MULTIFILE  = 0x08000000,
+        //! Plugin may produce multifile tracks
+        constexpr uint_t MULTIFILE = 0x08000000;
 
-          MASK       = 0x08000000
-        };
-        // clang-format on
+        constexpr uint_t MASK = 0x08000000;
       }  // namespace Traits
 
       static_assert(0 == (Category::MASK & Type::MASK & Device::MASK & Conversion::MASK & Traits::MASK),
@@ -160,35 +135,25 @@ namespace ZXTune
     {
       namespace Type
       {
-        // clang-format off
-        enum
-        {
-          ARCHIVE    = 0x00000000,
-          COMPRESSOR = 0x00000001,
-          SNAPSHOT   = 0x00000002,
-          DISKIMAGE  = 0x00000003,
-          DECOMPILER = 0x00000004,
-          MULTITRACK = 0x00000005,
-          SCANER     = 0x00000006,
+        constexpr uint_t ARCHIVE = 0x00000000;
+        constexpr uint_t COMPRESSOR = 0x00000001;
+        constexpr uint_t SNAPSHOT = 0x00000002;
+        constexpr uint_t DISKIMAGE = 0x00000003;
+        constexpr uint_t DECOMPILER = 0x00000004;
+        constexpr uint_t MULTITRACK = 0x00000005;
+        constexpr uint_t SCANER = 0x00000006;
 
-          MASK       = 0x0000000f
-        };
-        // clang-format on
+        constexpr uint_t MASK = 0x0000000f;
       }  // namespace Type
 
       namespace Traits
       {
-        // clang-format off
-        enum
-        {
-          //! Container may have directory structure
-          DIRECTORIES = 0x00000010,
-          //! Use plain transformation, can be covered by scaner
-          PLAIN       = 0x00000020,
+        //! Container may have directory structure
+        constexpr uint_t DIRECTORIES = 0x00000010;
+        //! Use plain transformation, can be covered by scaner
+        constexpr uint_t PLAIN = 0x00000020;
 
-          MASK        = 0x000000f0
-        };
-        // clang-format on
+        constexpr uint_t MASK = 0x000000f0;
       }  // namespace Traits
 
       static_assert(0 == (Category::MASK & Type::MASK & Traits::MASK), "Masks conflict");
