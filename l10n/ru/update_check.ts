609e2141a7620d16e242f623c6a1984b8cd0715f
vitamin-caig <vitamin.caig@gmail.com>
2024-12-09 16:48:19 +0300

    zxtune-qt: fix l10n files.

--- l10n/ru/update_check.ts
+++ l10n/ru/update_check.ts
@@ -4,22 +4,22 @@
 <context>
     <name>Update::CheckOperation</name>
     <message>
-        <location filename="../../apps/zxtune-qt/update/check.cpp" line="412"/>
+        <location filename="../../apps/zxtune-qt/update/check.cpp" line="413"/>
         <source>No new updates found</source>
         <translation>Обновлений нет</translation>
     </message>
     <message>
-        <location filename="../../apps/zxtune-qt/update/check.cpp" line="386"/>
+        <location filename="../../apps/zxtune-qt/update/check.cpp" line="387"/>
         <source>Getting list of available updates</source>
         <translation>Получение списка обновлений</translation>
     </message>
     <message>
-        <location filename="../../apps/zxtune-qt/update/check.cpp" line="430"/>
+        <location filename="../../apps/zxtune-qt/update/check.cpp" line="431"/>
         <source>Available version %1</source>
         <translation>Доступно обновление версии %1</translation>
     </message>
     <message numerus="yes">
-        <location filename="../../apps/zxtune-qt/update/check.cpp" line="435"/>
+        <location filename="../../apps/zxtune-qt/update/check.cpp" line="436"/>
         <source>%1 (%n day(s) ago)</source>
         <translation>
             <numerusform>%1 (%n день назад)</numerusform>
@@ -28,12 +28,12 @@
         </translation>
     </message>
     <message>
-        <location filename="../../apps/zxtune-qt/update/check.cpp" line="439"/>
+        <location filename="../../apps/zxtune-qt/update/check.cpp" line="440"/>
         <source>Download manually</source>
         <translation>Загрузить вручную</translation>
     </message>
     <message>
-        <location filename="../../apps/zxtune-qt/update/check.cpp" line="458"/>
+        <location filename="../../apps/zxtune-qt/update/check.cpp" line="459"/>
         <source>Downloading file</source>
         <translation>Загрузка файла</translation>
     </message>
