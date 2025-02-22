d86930b2a9aa605a163bfbd7d76b35671aec8296
vitamin-caig <vitamin.caig@gmail.com>
2025-02-22 21:43:25 +0300

    zxtune-android: fix compression support in HttpInputStream for tests.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/http/HttpInputStream.kt
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/fs/http/HttpInputStream.kt
@@ -2,11 +2,23 @@ package app.zxtune.fs.http
 
 import java.io.InputStream
 import java.net.HttpURLConnection
+import java.util.zip.GZIPInputStream
 
 internal class HttpInputStream(connection: HttpURLConnection) : InputStream() {
 
     private var connection: HttpURLConnection? = connection
-    private var delegate: InputStream? = connection.inputStream
+
+    // Just for tests - no transparent compression...
+    // https://developer.android.com/reference/java/net/HttpURLConnection.html#performance :
+    // By default, this implementation of HttpURLConnection requests that servers use gzip
+    // compression and it automatically decompresses the data for callers of
+    // URLConnection.getInputStream(). The Content-Encoding and Content-Length response headers
+    // are cleared in this case.
+    private var delegate: InputStream? = if ("gzip" == connection.contentEncoding) {
+        GZIPInputStream(connection.inputStream)
+    } else {
+        connection.inputStream
+    }
 
     override fun close() {
         delegate = null
