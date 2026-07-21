cf9b610fa9f6b985eda0e3925e974c6b4f7090b3
vitamin-caig <vitamin.caig@gmail.com>
2026-07-21 15:56:28 +0300

    he: port playback fix and channels muting support.
    https://github.com/myst6re/highly_experimental

--- 3rdparty/he/Core/spucore.h
+++ 3rdparty/he/Core/spucore.h
@@ -19,6 +19,8 @@ void   EMU_CALL spucore_clear_state(void *state);
 
 void   EMU_CALL spucore_set_mem_size(void *state, uint32 size);
 
+void   EMU_CALL spucore_set_mute_mask(void *state, uint32 mask);
+
 void   EMU_CALL spucore_render(void *state, uint16 *ram, sint16 *buf, sint16 *extinput, uint32 samples, uint8 mainout, uint8 effectout);
 
 uint32 EMU_CALL spucore_getreg      (void *state, uint32 n);
