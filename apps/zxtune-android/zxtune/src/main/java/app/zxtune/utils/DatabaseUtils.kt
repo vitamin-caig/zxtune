09a29de2704d90b257fb6d5b175ef2f24022e673
vitamin-caig <vitamin.caig@gmail.com>
2026-09-22 01:42:50 +0300

    zxtune-android: synchronize database moving to cache dir.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/utils/DatabaseUtils.kt
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/utils/DatabaseUtils.kt
@@ -5,6 +5,9 @@ import android.content.ContextWrapper
 import app.zxtune.Logger
 import java.io.File
 import java.io.IOException
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 
 val Context.withCachedDirDatabases: Context
     get() = CachedDirDatabaseContext(this)
@@ -12,6 +15,7 @@ val Context.withCachedDirDatabases: Context
 private class CachedDirDatabaseContext(base: Context) : ContextWrapper(base) {
     private val cachedDbs by lazy {
         File(super.cacheDir, "databases").apply {
+            LOG.d { "Dbs at $this exists=$isDirectory" }
             if (!isDirectory && !mkdirs()) {
                 LOG.w(IOException(absolutePath)) {
                     "Failed to create directory"
@@ -19,28 +23,33 @@ private class CachedDirDatabaseContext(base: Context) : ContextWrapper(base) {
             }
         }
     }
+    private val lock = ReentrantReadWriteLock()
 
-    override fun getDatabasePath(name: String): File {
+    override fun getDatabasePath(name: String): File = lock.read {
         val legacy = super.getDatabasePath(name)
         val modern = File(cachedDbs, name)
         if (legacy.isFile && (!modern.isFile || legacy.lastModified() > modern.lastModified())) {
             LOG.d {
                 "Migrate ${legacy.absolutePath} -> ${modern.absolutePath} (${legacy.length()})"
             }
-            for (suffix in arrayOf("", "-shm", "-wal", "-journal")) {
-                val src = File(legacy.absolutePath + suffix)
-                if (!src.isFile) {
-                    continue
-                }
-                val dst = File(modern.absolutePath + suffix).apply {
-                    deleteRecursively()
-                }
-                if (!src.renameTo(dst)) {
-                    LOG.d { " Failed to rename ${src.absolutePath}" }
-                    if (suffix.isEmpty()) {
-                        return legacy
+            lock.write {
+                for (suffix in arrayOf("", "-shm", "-wal", "-journal")) {
+                    val src = File(legacy.absolutePath + suffix)
+                    if (!src.isFile) {
+                        continue
                     }
-                    src.delete()
+                    val dst = File(modern.absolutePath + suffix).apply {
+                        deleteRecursively()
+                    }
+                    LOG.d { "Rename $src -> $dst" }
+                    if (!src.renameTo(dst)) {
+                        LOG.d { " Failed to rename ${src.absolutePath}" }
+                        if (suffix.isEmpty()) {
+                            LOG.d { "Use legacy $src" }
+                            return legacy
+                        }
+                        src.delete()
+                    }
                 }
             }
         }
